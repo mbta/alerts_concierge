c0a6a4b7a4007e5193d522dc700d1d8e7ef7be85
faizaanshamsi <dev@faizaanshamsi.com>
2017-04-03 15:42:37 -0400

    Temporary pipeline from alerts to messages
    1. We can parse alerts from the API
    2. We pass them through a dummy filter
    3. We can send them out via AWS email + text

---
+++ lib/mbta_server/alert_processor/rules_engine/subscription_filter_engine.ex
@@ -0,0 +1,27 @@
+defmodule MbtaServer.AlertProcessor.SubscriptionFilterEngine do
+  @moduledoc """
+  Entry point for susbcription engine to filter users to alert users
+  with relevant subscriptions to alert provided.
+  """
+  alias MbtaServer.AlertProcessor.{Messager}
+
+  @type alert :: %{
+    header: String.t
+  }
+
+  @doc """
+  process_alert/1 receives an alert and applies relevant filters to send alerts
+  to the correct users based on the alert.
+  """
+  @spec process_alert(alert, %MbtaServer.User{} | nil) :: {:ok, Map} | {:error, Map} | {:error, String.t}
+  def process_alert(alert, user \\ test_user()) do
+    %{email: email, phone_number: phone_number} = user
+    Messager.send_alert_message({alert[:header], email, phone_number})
+  end
+
+  @spec test_user :: %MbtaServer.User{}
+  defp test_user do
+    import Ecto.Query
+    MbtaServer.Repo.one(from x in MbtaServer.User, order_by: [asc: x.id], limit: 1)
+  end
+end
