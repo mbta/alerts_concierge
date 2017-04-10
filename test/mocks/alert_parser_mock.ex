970da88fda121499c2676495ec03ef9165eb533c
Brian Fauble <bfauble@intrepid.io>
2017-04-10 17:03:39 -0400

    add doc for alert parser mock

--- test/mocks/alert_parser_mock.ex
+++ test/mocks/alert_parser_mock.ex
@@ -1,4 +1,13 @@
 defmodule MbtaServer.AlertProcessor.AlertParserMock do
+  @moduledoc """
+  Module to act as mock implementation for AlertParser
+  for testing purposes.
+  """
+
+  @doc """
+  process_alerts/1 send messages to self for tests to be able
+  to verify that process_alerts has properly been called.
+  """
   def process_alerts do
     send self(), :processed_alerts
   end
