ac3dec2283d93d1f112b4e1713ba2a2f2f8b5fa3
faizaanshamsi <dev@faizaanshamsi.com>
2017-04-18 16:51:28 -0400

    refactor tests

--- test/alert_processor/rules_engine/sent_alert_filter_test.exs
+++ test/alert_processor/rules_engine/sent_alert_filter_test.exs
@@ -5,56 +5,54 @@ defmodule MbtaServer.AlertProcessor.SentAlertFilterTest do
   alias Model.{Alert, Notification}
   import MbtaServer.Factory
 
-  setup do
-    alert = %Alert{id: "123"}
+  @alert %Alert{id: "123"}
 
-    {:ok, alert: alert}
-  end
+  describe "filter/1" do
+    test "returns all users who have not received the alert" do
+      user = insert(:user)
+      other_notification_user = insert(:user)
+      notified_user = insert(:user)
 
-  test "filter/2 returns all users who have not received the alert", %{alert: alert} do
-    user = insert(:user)
-    other_notification_user = insert(:user)
-    notified_user = insert(:user)
+      notification = %Notification{
+        alert_id: "123",
+        user_id: notified_user.id,
+        email: "a@b.com",
+        message: "You are being notified",
+        status: :sent
+      }
 
-    notification = %Notification{
-      alert_id: "123",
-      user_id: notified_user.id,
-      email: "a@b.com",
-      message: "You are being notified",
-      status: :sent
-    }
+      other_notification = %Notification{
+        alert_id: "456",
+        user_id: other_notification_user.id,
+        email: "c@d.com",
+        message: "You have been notified",
+        status: :sent
+      }
 
-    other_notification = %Notification{
-      alert_id: "456",
-      user_id: other_notification_user.id,
-      email: "c@d.com",
-      message: "You have been notified",
-      status: :sent
-    }
+      Repo.insert(Notification.create_changeset(notification))
+      Repo.insert(Notification.create_changeset(other_notification))
 
-    Repo.insert(Notification.create_changeset(notification))
-    Repo.insert(Notification.create_changeset(other_notification))
+      assert {:ok, [user.id, other_notification_user.id], @alert} == SentAlertFilter.filter(@alert)
+    end
 
-    assert {:ok, [user.id, other_notification_user.id], alert} == SentAlertFilter.filter(alert)
-  end
+    test "returns empty list if no match" do
+      assert {:ok, [], @alert} == SentAlertFilter.filter(@alert)
+    end
 
-  test "filter/2 error if no match", %{alert: alert} do
-    assert {:ok, [], alert} == SentAlertFilter.filter(alert)
-  end
+    test "returns the user if notification failed" do
+      user = insert(:user)
 
-  test "filter/2 return user if notification failed", %{alert: alert} do
-    user = insert(:user)
+      notification = %Notification{
+        alert_id: "123",
+        user_id: user.id,
+        email: "a@b.com",
+        message: "You are being notified",
+        status: :failed
+      }
 
-    notification = %Notification{
-      alert_id: "123",
-      user_id: user.id,
-      email: "a@b.com",
-      message: "You are being notified",
-      status: :failed
-    }
+      Repo.insert(Notification.create_changeset(notification))
 
-    Repo.insert(Notification.create_changeset(notification))
-
-    assert {:ok, [user.id], alert} == SentAlertFilter.filter(alert)
+      assert {:ok, [user.id], @alert} == SentAlertFilter.filter(@alert)
+    end
   end
 end
