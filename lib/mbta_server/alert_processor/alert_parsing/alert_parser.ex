3644dc759b7c22d8f96ab0959404a0624e2831a8
Brian Fauble <bfauble@intrepid.io>
2017-04-20 15:48:50 -0400

    add effect_name to items alert parser parses from alert json

--- lib/mbta_server/alert_processor/alert_parsing/alert_parser.ex
+++ lib/mbta_server/alert_processor/alert_parsing/alert_parser.ex
@@ -4,6 +4,7 @@ defmodule MbtaServer.AlertProcessor.AlertParser do
   relevant information to subscription filter engine.
   """
   alias MbtaServer.AlertProcessor.{AlertCache, ApiClient, HoldingQueue, Parser, SubscriptionFilterEngine}
+  alias MbtaServer.AlertProcessor.Model.Alert
 
   @behaviour Parser
 
@@ -26,9 +27,29 @@ defmodule MbtaServer.AlertProcessor.AlertParser do
     end
   end
 
-  @spec parse_alert(map, map) :: [%{String.t => %{header: String.t, alert_id: String.t}}]
-  defp parse_alert(%{"attributes" => %{"header" => header, "informed_entity" => informed_entities, "severity" => severity}, "id" => alert_id}, accumulator) when is_binary(header) do
-    Map.put(accumulator, alert_id, %{header: header, alert_id: alert_id, informed_entities: informed_entities, severity: severity |> String.downcase |> String.to_existing_atom})
+  @spec parse_alert(map, map) :: [%{String.t => Alert.t}]
+  defp parse_alert(
+    %{
+      "attributes" => %{
+        "effect_name" => effect_name,
+        "header" => header,
+        "informed_entity" => informed_entities,
+        "severity" => severity
+      },
+      "id" => alert_id
+    }, accumulator
+  ) when is_binary(header) do
+    Map.put(
+      accumulator,
+      alert_id,
+      struct(Alert, %{
+        id: alert_id,
+        effect_name: effect_name,
+        header: header,
+        informed_entities: informed_entities,
+        severity: severity |> String.downcase |> String.to_existing_atom
+      })
+    )
   end
 
   defp parse_alert(_, accumulator) do
