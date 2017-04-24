c106ca8a46d84cb467660d930045b27084bba4cd
Brian Fauble <bfauble@intrepid.io>
2017-04-24 12:24:13 -0400

    refactor informed entity filter to return queryable instead of list

--- lib/mbta_server/alert_processor/rules_engine/informed_entity_filter.ex
+++ lib/mbta_server/alert_processor/rules_engine/informed_entity_filter.ex
@@ -4,8 +4,7 @@ defmodule MbtaServer.AlertProcessor.InformedEntityFilter do
   """
 
   import Ecto.Query
-  alias MbtaServer.Repo
-  alias MbtaServer.AlertProcessor.Model.{Alert, InformedEntity, Subscription}
+  alias MbtaServer.AlertProcessor.Model.{Alert, InformedEntity}
 
   @doc """
   filter/1 takes a tuple of the remaining subscriptions to be considered and
@@ -14,23 +13,20 @@ defmodule MbtaServer.AlertProcessor.InformedEntityFilter do
   an alert to pass through to the next filter. Otherwise the flow is
   shortcircuited if the user id list provided is missing or empty.
   """
-  @spec filter({:ok, [Subscription.id], Alert.t}) :: {:ok, [Subscription.id], Alert.t}
-  def filter({:ok, [], %Alert{} = alert}), do: {:ok, [], alert}
-  def filter({:ok, subscription_ids, %Alert{informed_entities: informed_entities} = alert}) do
+  @spec filter({:ok, Ecto.Queryable.t, Alert.t}) :: {:ok, Ecto.Queryable.t, Alert.t}
+  def filter({:ok, previous_query, %Alert{informed_entities: informed_entities} = alert}) do
     where_clause =
       Enum.reduce(informed_entities, false, fn(informed_entity), dynamic_query ->
         informed_entity_where_clause = informed_entity_where_clause(struct(InformedEntity, informed_entity))
         dynamic(^informed_entity_where_clause or ^dynamic_query)
       end)
 
-    query = from ie in InformedEntity,
-      join: s in Subscription,
-      on: s.id == ie.subscription_id,
-      select: s.id,
-      where: s.id in ^subscription_ids,
+    query = from s in subquery(previous_query),
+      join: ie in InformedEntity,
+      where: s.id == ie.subscription_id,
       where: ^where_clause
 
-    {:ok, Repo.all(query), alert}
+    {:ok, query, alert}
   end
 
   defp informed_entity_where_clause(informed_entity) do
@@ -38,9 +34,9 @@ defmodule MbtaServer.AlertProcessor.InformedEntityFilter do
     |> Map.take(InformedEntity.queryable_fields)
     |> Enum.reduce(true, fn({k, v}, dynamic_query) ->
       if v do
-        dynamic([ie], field(ie, ^k) == ^v and ^dynamic_query)
+        dynamic([s, ie], field(ie, ^k) == ^v and ^dynamic_query)
       else
-        dynamic([ie], is_nil(field(ie, ^k)) and ^dynamic_query)
+        dynamic([s, ie], is_nil(field(ie, ^k)) and ^dynamic_query)
       end
     end)
   end
