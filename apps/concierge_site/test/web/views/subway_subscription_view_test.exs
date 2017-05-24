a1f518992ab978b7fa837d4fd37407b3ef4c887c
Danny Rosenblatt <dgrosenblatt@gmail.com>
2017-05-24 08:46:33 -0400

    Add subway flow progress bar

---
+++ apps/concierge_site/test/web/views/subway_subscription_view_test.exs
@@ -0,0 +1,36 @@
+defmodule ConciergeSite.SubwaySubscriptionViewTest do
+  use ExUnit.Case
+  alias ConciergeSite.SubwaySubscriptionView
+
+  describe "progress_link_class" do
+    test "it returns the disabled class when the page is trip_type" do
+      class = SubwaySubscriptionView.progress_link_class(:trip_type, :trip_info)
+      assert class === "disabled-progress-link"
+    end
+
+    test "it returns the disabled class when the page/step are included in disabled_progress_bar_links" do
+      class = SubwaySubscriptionView.progress_link_class(:trip_info, :preferences)
+      assert class === "disabled-progress-link"
+    end
+
+    test "it returns nil otherwise" do
+      class = SubwaySubscriptionView.progress_link_class(:trip_info, :trip_type)
+      assert class === nil
+    end
+  end
+
+  describe "progress_step_classes" do
+
+    @active_classes %{circle: "active-circle", name: "active-page"}
+
+    test "it returns a map of active classes when step and page are equal" do
+      classes = SubwaySubscriptionView.progress_step_classes(:trip_info, :trip_info)
+      assert classes === @active_classes
+    end
+
+    test "it returns an empty map when step and page are different" do
+      classes = SubwaySubscriptionView.progress_step_classes(:preferences, :trip_type)
+      assert classes === %{}
+    end
+  end
+end
