dfa2c6ac26365f614f953d3a758fee9821fb15a7
Danny Rosenblatt <dgrosenblatt@gmail.com>
2017-05-24 08:42:44 -0400

    Add select Trip Type page

--- apps/concierge_site/test/web/controllers/subway_subscription_controller_test.exs
+++ apps/concierge_site/test/web/controllers/subway_subscription_controller_test.exs
@@ -15,13 +15,29 @@ defmodule ConciergeSite.SubwaySubscriptionControllerTest do
       |> guardian_login(conn)
       |> get("/subscriptions/subway/new")
 
-      assert html_response(conn, 200) =~ "Subway"
+      assert html_response(conn, 200) =~ "What type of trip do you take?"
+    end
+
+    test "GET /subscriptions/subway/new/info", %{conn: conn}  do
+      user = Repo.insert!(%User{email: "test@email.com",
+                                role: "user",
+                                encrypted_password: @encrypted_password})
+      conn = user
+      |> guardian_login(conn)
+      |> get("/subscriptions/subway/new/info")
+
+      assert html_response(conn, 200) =~ "Info"
     end
   end
 
   describe "unauthorized" do
     test "GET /subscriptions/subway/new", %{conn: conn} do
       conn = get(conn, "/subscriptions/subway/new")
+      assert html_response(conn, 302) =~ "/login"
+    end
+
+    test "GET /subscriptions/subway/new/info", %{conn: conn} do
+      conn = get(conn, "/subscriptions/subway/new/info")
       assert html_response(conn, 302) =~ "/login"
     end
   end
