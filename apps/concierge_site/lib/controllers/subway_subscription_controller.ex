e57a6edc2a4e1a89c4614dc869e4a1a2c36bdd35
Danny Rosenblatt <dgrosenblatt@gmail.com>
2017-06-06 10:02:32 -0400

    Display error message when subway line fetch fails

--- apps/concierge_site/lib/controllers/subway_subscription_controller.ex
+++ apps/concierge_site/lib/controllers/subway_subscription_controller.ex
@@ -2,6 +2,7 @@ defmodule ConciergeSite.SubwaySubscriptionController do
   use ConciergeSite.Web, :controller
   use Guardian.Phoenix.Controller
   alias ConciergeSite.Subscriptions.TemporaryState
+  alias ConciergeSite.Subscriptions.SubwayLines
   alias AlertProcessor.ServiceInfoCache
 
   def new(conn, _params, _user, _claims) do
@@ -11,11 +12,20 @@ defmodule ConciergeSite.SubwaySubscriptionController do
   def info(conn, params, user, _claims) do
     subscription_params = Map.merge(params, %{user_id: user.id, route_type: 1})
     token = TemporaryState.encode(subscription_params)
-    {:ok, stations} = ServiceInfoCache.get_subway_info
-    
-    render conn, "info.html",
-      token: token,
-      subscription_params: subscription_params,
-      stations: stations
+
+    case ServiceInfoCache.get_subway_info do
+      {:ok, stations} ->
+        station_list_select_options =
+            SubwayLines.station_list_select_options(stations)
+
+        render conn, "info.html",
+          token: token,
+          subscription_params: subscription_params,
+          station_list_select_options: station_list_select_options
+      _error ->
+        conn
+        |> put_flash(:error, "There was an error fetching station data. Please try again.")
+        |> render("new.html", token: token, subscription_params: subscription_params)
+    end
   end
 end
