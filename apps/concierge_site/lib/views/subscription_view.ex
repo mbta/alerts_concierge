bd2a6bc70cfb60820b3e0b77c895631266b532c5
Brian Fauble <bfauble@intrepid.io>
2017-06-08 09:53:47 -0400

    add subscription view for grouping, sorting and displaying subscriptions for a user

--- apps/concierge_site/lib/views/subscription_view.ex
+++ apps/concierge_site/lib/views/subscription_view.ex
@@ -1,3 +1,130 @@
 defmodule ConciergeSite.SubscriptionView do
   use ConciergeSite.Web, :view
+
+  alias AlertProcessor.Helpers.DateTimeHelper
+  alias AlertProcessor.Model.InformedEntity
+
+  def sorted_subscriptions(subscriptions) do
+    with grouped_subscriptions <- group_subscriptions_by_mode_route_and_stations(subscriptions),
+         sorted_by_earliest_time <- order_subscriptions_for_stations_by_earliest_time(grouped_subscriptions),
+         subscription_map <- flatten_arrays(sorted_by_earliest_time) do
+           Map.merge(%{amenity: [], boat: [], bus: [], commuter_rail: [], subway: []}, subscription_map)
+    end
+  end
+
+  defp group_subscriptions_by_mode_route_and_stations(subscriptions) do
+    Enum.reduce(subscriptions, %{}, fn(subscription, acc) ->
+      route_key = subscription |> parse_route() |> String.split("-") |> List.first()
+      origin_dest_key = Enum.sort([subscription.origin, subscription.destination]) |> Enum.join()
+      update = %{{subscription.type, route_key, origin_dest_key} => [subscription]}
+
+      Map.merge(acc, update, fn(_, s1, s2) ->
+        Enum.sort_by(s1 ++ s2, &(&1.start_time))
+      end)
+    end)
+  end
+
+  defp order_subscriptions_for_stations_by_earliest_time(grouped_subscriptions) do
+    Enum.reduce(grouped_subscriptions, %{}, fn({{type, line, _dest_origin}, subscriptions}, acc) ->
+      update = %{{type, line} => [subscriptions]}
+
+      Map.merge(acc, update, fn(_, curr_subs, new_subs) ->
+        Enum.sort_by(curr_subs ++ new_subs, fn(subscriptions) ->
+          [earliest_subscription | _] = subscriptions
+          earliest_subscription.start_time
+        end)
+      end)
+    end)
+  end
+
+  defp flatten_arrays(sorted_by_earliest_time) do
+    Enum.reduce(sorted_by_earliest_time, %{}, fn({{type, _line}, subscriptions}, acc) ->
+      flattened_subscriptions = Enum.flat_map(subscriptions, &(&1))
+
+      Map.update(acc, type, flattened_subscriptions, fn(subs) ->
+        subs ++ flattened_subscriptions
+      end)
+    end)
+  end
+
+  def subway_subscription_info(subscription) do
+    content_tag :div, class: "subscription-info" do
+      [
+        content_tag :div, class: "subscription-route" do
+          route_header(subscription)
+        end,
+        content_tag :div, class: "subscription-details" do
+          route_body(subscription)
+        end
+      ]
+    end
+  end
+
+  defp route_header(subscription) do
+    case direction_name(subscription) do
+      :roaming -> content_tag(:span, "#{subscription.origin} - #{subscription.destination}")
+      _ -> [
+            content_tag(:span, subscription.origin),
+            content_tag(:i, "", class: "fa fa-long-arrow-right"),
+            content_tag(:span, subscription.destination)
+          ]
+    end
+  end
+
+  defp route_body(subscription) do
+    case direction_name(subscription) do
+      :roaming ->
+        timeframe(subscription)
+      _ ->
+        "#{timeframe(subscription)} | #{direction_name(subscription)} #{parse_headsign(subscription)}"
+    end
+  end
+
+  defp timeframe(subscription) do
+    "#{pretty_time(subscription.start_time)} - #{pretty_time(subscription.end_time)}, \
+    #{Enum.map_join(subscription.relevant_days, ", ", & &1 |> Atom.to_string() |> String.capitalize())}"
+  end
+
+  defp pretty_time(timestamp) do
+    with local_time <- DateTimeHelper.utc_time_to_local(timestamp),
+      {:ok, output} <- Calendar.Strftime.strftime(local_time, "%l:%M%P") do
+      output
+    end
+  end
+
+  defp parse_route(subscription) do
+    %InformedEntity{route: route} = Enum.find(subscription.informed_entities, fn(%InformedEntity{route: route}) -> !is_nil(route) end)
+    route
+  end
+
+  defp direction_name(subscription) do
+    case parse_direction(subscription) do
+      :roaming ->
+        :roaming
+      direction ->
+        {:ok, direction_name} = AlertProcessor.ServiceInfoCache.get_direction_name(:subway, parse_route(subscription), direction)
+        direction_name
+    end
+  end
+
+  defp parse_direction(subscription) do
+    subscription.informed_entities
+    |> Enum.filter_map(&(!is_nil(&1.direction_id)), &(&1.direction_id))
+    |> Enum.uniq()
+    |> case do
+      [id | []] -> id
+      _ -> :roaming
+    end
+  end
+
+  defp parse_headsign(subscription) do
+    direction = parse_direction(subscription)
+    case direction do
+      nil ->
+        ""
+      direction ->
+        {:ok, headsign} = AlertProcessor.ServiceInfoCache.get_headsign(:subway, subscription.origin, subscription.destination, direction)
+        " to " <> headsign
+    end
+  end
 end
---
+++ apps/concierge_site/test/web/views/subscription_view_test.exs
@@ -0,0 +1,33 @@
+defmodule ConciergeSite.SubscriptionViewTest do
+  use ExUnit.Case
+  alias ConciergeSite.SubscriptionView
+  alias AlertProcessor.Model.{InformedEntity, Subscription}
+
+  test "sorted_subscriptions groups subscriptions by mode" do
+    sub1 = %Subscription{origin: "Davis", destination: "Park Street", type: :subway, informed_entities: [%InformedEntity{route: "Red", route_type: 1}]}
+    sub2 = %Subscription{type: :bus, informed_entities: [%InformedEntity{route: "1", route_type: 3}]}
+    sub3 = %Subscription{type: :commuter_rail, informed_entities: [%InformedEntity{route: "CR-Lowell", route_type: 2}]}
+    assert %{amenity: [], boat: [], bus: [^sub2], commuter_rail: [^sub3], subway: [^sub1]} = SubscriptionView.sorted_subscriptions([sub1, sub2, sub3])
+  end
+
+  test "sorted_subscriptions groups subscriptions by line" do
+    sub1 = %Subscription{origin: "Davis", destination: "Park Street", type: :subway, informed_entities: [%InformedEntity{route: "Red", route_type: 1}]}
+    sub2 = %Subscription{origin: "Oak Grove", destination: "Downtown Crossing", type: :subway, informed_entities: [%InformedEntity{route: "Orange", route_type: 1}]}
+    sub3 = %Subscription{origin: "Park Street", destination: "Davis", type: :subway, informed_entities: [%InformedEntity{route: "Red", route_type: 1}]}
+    assert %{amenity: [], boat: [], bus: [], commuter_rail: [], subway: [^sub2, ^sub1, ^sub3]} = SubscriptionView.sorted_subscriptions([sub1, sub2, sub3])
+  end
+
+  test "sorted_subscriptions groups subscriptions by origin/destination combos" do
+    sub1 = %Subscription{origin: "Davis", destination: "Park Street", type: :subway, informed_entities: [%InformedEntity{route: "Red", route_type: 1}]}
+    sub2 = %Subscription{origin: "Davis", destination: "Harvard", type: :subway, informed_entities: [%InformedEntity{route: "Red", route_type: 1}]}
+    sub3 = %Subscription{origin: "Park Street", destination: "Davis", type: :subway, informed_entities: [%InformedEntity{route: "Red", route_type: 1}]}
+    assert %{amenity: [], boat: [], bus: [], commuter_rail: [], subway: [^sub2, ^sub1, ^sub3]} = SubscriptionView.sorted_subscriptions([sub1, sub2, sub3])
+  end
+
+  test "sorted_subscriptions sorts by earliest start_time" do
+    sub1 = %Subscription{origin: "Davis", destination: "Park Street", type: :subway, start_time: ~T[14:00:00], end_time: ~T[15:00:00], informed_entities: [%InformedEntity{route: "Red", route_type: 1}]}
+    sub2 = %Subscription{origin: "Davis", destination: "Harvard", type: :subway, start_time: ~T[12:00:00], end_time: ~T[13:00:00], informed_entities: [%InformedEntity{route: "Red", route_type: 1}]}
+    sub3 = %Subscription{origin: "Park Street", destination: "Davis", type: :subway, start_time: ~T[10:00:00], end_time: ~T[11:00:00], informed_entities: [%InformedEntity{route: "Red", route_type: 1}]}
+    assert %{amenity: [], boat: [], bus: [], commuter_rail: [], subway: [^sub3, ^sub1, ^sub2]} = SubscriptionView.sorted_subscriptions([sub1, sub2, sub3])
+  end
+end
