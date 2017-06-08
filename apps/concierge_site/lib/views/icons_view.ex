00ba0fd8d8f725f518bf5e5197f7c61a1409e824
Brian Fauble <bfauble@intrepid.io>
2017-06-08 16:25:07 -0400

    pass in %Route{} to icon helper partial

--- apps/concierge_site/lib/views/icons_view.ex
+++ apps/concierge_site/lib/views/icons_view.ex
@@ -1,15 +1,17 @@
 defmodule ConciergeSite.IconsView do
   use ConciergeSite.Web, :view
 
-  def render("_circle_icon.html", %{route: route, large: true}) do
-    downcase_route_name = route |> String.split() |> List.first() |> String.downcase()
+  alias AlertProcessor.Model.Route
 
-    render "_circle_icon.html", %{svg_class: "large-icon-with-circle", title: route, circle_class: ["icon-", downcase_route_name, "-line-circle"]}
+  def render("_circle_icon.html", %{route: %Route{long_name: long_name}, large: true}) do
+    downcase_route_name = long_name |> String.split() |> List.first() |> String.downcase()
+
+    render "_circle_icon.html", %{svg_class: "large-icon-with-circle", title: long_name, circle_class: ["icon-", downcase_route_name, "-line-circle"]}
   end
 
-  def render("_circle_icon.html", %{route: route}) do
-    downcase_route_name = route |> String.split() |> List.first() |> String.downcase()
+  def render("_circle_icon.html", %{route: %Route{long_name: long_name}}) do
+    downcase_route_name = long_name |> String.split() |> List.first() |> String.downcase()
 
-    render "_circle_icon.html", %{svg_class: "icon-with-circle", title: route, circle_class: ["icon-", downcase_route_name, "-line-circle"]}
+    render "_circle_icon.html", %{svg_class: "icon-with-circle", title: long_name, circle_class: ["icon-", downcase_route_name, "-line-circle"]}
   end
 end
--- apps/concierge_site/test/web/views/icons_view_test.exs
+++ apps/concierge_site/test/web/views/icons_view_test.exs
@@ -3,15 +3,17 @@ defmodule ConciergeSite.IconsViewTest do
 
   import Phoenix.View
 
+  alias AlertProcessor.Model.Route
+
   test "renders icon partial" do
-    rendered_template = render_to_string(ConciergeSite.IconsView, "_circle_icon.html", %{route: "Green Line B"})
+    rendered_template = render_to_string(ConciergeSite.IconsView, "_circle_icon.html", %{route: %Route{long_name: "Green Line B"}})
     assert rendered_template =~ ~s(title="Green Line B")
     assert rendered_template =~ ~s(class="icon-green-line-circle")
     assert rendered_template =~ ~s(class="icon-with-circle")
   end
 
   test "renders large icon partial" do
-    rendered_template = render_to_string(ConciergeSite.IconsView, "_circle_icon.html", %{route: "Red Line", large: true})
+    rendered_template = render_to_string(ConciergeSite.IconsView, "_circle_icon.html", %{route: %Route{long_name: "Red Line"}, large: true})
     assert rendered_template =~ ~s(title="Red Line")
     assert rendered_template =~ ~s(class="icon-red-line-circle")
     assert rendered_template =~ ~s(class="large-icon-with-circle")
