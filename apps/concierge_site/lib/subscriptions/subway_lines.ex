da6b8c581c9de421c4e18f34689a4af989f96668
Danny Rosenblatt <dgrosenblatt@gmail.com>
2017-06-12 14:49:41 -0400

    Return Braintree and Ashmont Red Line names for select options

--- apps/concierge_site/lib/subscriptions/subway_lines.ex
+++ apps/concierge_site/lib/subscriptions/subway_lines.ex
@@ -12,6 +12,19 @@ defmodule ConciergeSite.Subscriptions.SubwayLines do
 
   @spec station_list_select_options(%AlertProcessor.Model.Route{}) :: [{String.t, list}]
   def station_list_select_options(routes) do
-    Enum.map(routes, fn(route) -> {route.long_name, route.stop_list} end)
+    Enum.map(routes, fn(route) ->
+      [{first_stop_name, first_stop_code} | _stops] = route.stop_list
+
+      route_name =
+        case {first_stop_name, first_stop_code, route.long_name} do
+          {"Ashmont", "place-asmnl", "Red Line"} ->
+            "Red Line Ashmont"
+          {"Braintree", "place-brntn", "Red Line"} ->
+            "Red Line Braintree"
+          {_stop_name, _stop_code, _route_name} ->
+            route.long_name
+        end
+      {route_name, route.stop_list}
+    end)
   end
 end
--- apps/concierge_site/test/web/subscriptions/subway_lines_test.exs
+++ apps/concierge_site/test/web/subscriptions/subway_lines_test.exs
@@ -5,14 +5,77 @@ defmodule ConciergeSite.Subscriptions.SubwayLinesTest do
   describe "station_select_list_options" do
     test "changes a map of routes into a keyword list" do
       routes = [
+<<<<<<< 41fef6e539de13df2bf7766a4e5b566084d0e377
         %AlertProcessor.Model.Route{direction_names: [], long_name: "Green Line D", route_type: 0, stop_list: []},
         %AlertProcessor.Model.Route{direction_names: [], long_name: "Red Line", route_type: 0, stop_list: []},
         %AlertProcessor.Model.Route{direction_names: [], long_name: "Blue Line", route_type: 0, stop_list: []}
+=======
+        %AlertProcessor.Model.Route{
+          direction_names: [],
+          long_name: "Green Line D",
+          route_type: 0,
+          stop_list: [{"Riverside", "place-river"}]
+        },
+        %AlertProcessor.Model.Route{
+          direction_names: [],
+          long_name: "Orange Line",
+          route_type: 0,
+          stop_list: [{"Forest Hills", "place-forhl"}]
+        },
+        %AlertProcessor.Model.Route{
+          direction_names: [],
+          long_name: "Blue Line",
+          route_type: 0,
+          stop_list: [{"Bowdoin", "place-bomnl"}]
+        }
+>>>>>>> Return Braintree and Ashmont Red Line names for select options
       ]
 
       select_options = SubwayLines.station_list_select_options(routes)
 
-      assert select_options == [{"Green Line D", []}, {"Red Line", []}, {"Blue Line", []}]
+      assert select_options == [
+        {"Green Line D", [{"Riverside", "place-river"}]},
+        {"Orange Line", [{"Forest Hills", "place-forhl"}]},
+        {"Blue Line", [{"Bowdoin", "place-bomnl"}]}
+      ]
+    end
+
+    test "names the Red Line Ashmont and Braintree branches" do
+      routes = [
+        %AlertProcessor.Model.Route{
+          direction_names: [],
+          long_name: "Red Line",
+          route_type: 0,
+          stop_list: [
+            {"Ashmont", "place-asmnl"},
+            {"Shawmut", "place-smmnl"}
+          ]
+        },
+        %AlertProcessor.Model.Route{
+          direction_names: [],
+          long_name: "Red Line",
+          route_type: 0,
+          stop_list: [
+            {"Braintree", "place-brntn"},
+            {"Quincy Adams", "place-qamnl"}
+          ]
+        },
+        %AlertProcessor.Model.Route{
+          direction_names: [],
+          long_name: "Orange Line",
+          route_type: 0,
+          stop_list: [{"Forest Hills", "place-forhl"}
+          ]
+        }
+      ]
+
+      select_options = SubwayLines.station_list_select_options(routes)
+
+      assert select_options == [
+        {"Red Line Ashmont", [{"Ashmont", "place-asmnl"}, {"Shawmut", "place-smmnl"}]},
+        {"Red Line Braintree", [{"Braintree", "place-brntn"}, {"Quincy Adams", "place-qamnl"}]},
+        {"Orange Line", [{"Forest Hills", "place-forhl"}]}
+      ]
     end
   end
 end
