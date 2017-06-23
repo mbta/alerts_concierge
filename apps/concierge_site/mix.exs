a8ea577b794c0efd717cf0df1beff0e5de12763a
Brian Fauble <bfauble@intrepid.io>
2017-06-23 10:53:51 -0400

    bump to version 0.0.8

--- apps/concierge_site/mix.exs
+++ apps/concierge_site/mix.exs
@@ -3,7 +3,7 @@ defmodule ConciergeSite.Mixfile do
 
   def project do
     [app: :concierge_site,
-     version: "0.0.7",
+     version: "0.0.8",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
