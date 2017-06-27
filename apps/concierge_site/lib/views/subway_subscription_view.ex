4be83b6bfcbfc1f1a90f40d474d541a2e7a0ff70
Danny Rosenblatt <dgrosenblatt@gmail.com>
2017-06-27 11:12:41 -0400

    Fix merge conflict

--- apps/concierge_site/lib/views/subway_subscription_view.ex
+++ apps/concierge_site/lib/views/subway_subscription_view.ex
@@ -139,16 +139,4 @@ defmodule ConciergeSite.SubwaySubscriptionView do
       end)
     |> StringHelper.or_join()
   end
-
-  @doc """
-  Returns stringified times to populate a dropdown list of a full day of times at
-  fifteen-minute intervals
-  """
-  def travel_time_options() do
-    0
-    |> Stream.iterate(&(&1 + 900))
-    |> Stream.map(&Calendar.Time.from_second_in_day/1)
-    |> Stream.map(&Calendar.Strftime.strftime!(&1, "%I:%M %p"))
-    |> Enum.take(96)
-  end
 end
