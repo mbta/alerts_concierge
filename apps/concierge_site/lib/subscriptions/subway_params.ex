7ea5fe11b9f6a9b80aaf49074055ac2bf1e6240b
Danny Rosenblatt <dgrosenblatt@gmail.com>
2017-06-29 17:34:19 -0400

    Add helpers for Subscription changeset

--- apps/concierge_site/lib/subscriptions/subway_params.ex
+++ apps/concierge_site/lib/subscriptions/subway_params.ex
@@ -123,11 +123,25 @@ defmodule ConciergeSite.Subscriptions.SubwayParams do
     |> Map.merge(translated_params)
   end
 
+  @doc """
+  """
+  @spec prepare_for_update_changeset(map) :: map
+  def prepare_for_update_changeset(params) do
+    relevant_days =
+      params
+      |> Map.take(~w(saturday sunday weekday))
+      |> relevant_days_from_booleans()
+      |> Enum.map(&String.to_existing_atom/1)
+
+    %{"alert_priority_type" => String.to_existing_atom(params["alert_priority_type"]),
+      "relevant_days" => relevant_days,
+      "end_time" => params["departure_end"],
+      "start_time" => params["departure_start"]}
+  end
+
   defp relevant_days_from_booleans(day_map) do
     day_map
-    |> Enum.filter_map(
-      fn {_day, bool} -> bool == "true" end,
-      fn {day, _bool} -> day end
-    )
+    |> Enum.filter(fn {_day, bool} -> bool == "true" end)
+    |> Enum.map(fn {day, _bool} -> day end)
   end
 end
