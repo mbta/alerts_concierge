284a691fcc571316b360897f1738b5bd192b9964
Danny Rosenblatt <dgrosenblatt@gmail.com>
2017-07-11 10:25:51 -0400

    Move current password authorization into plug

--- apps/concierge_site/lib/controllers/password_controller.ex
+++ apps/concierge_site/lib/controllers/password_controller.ex
@@ -4,29 +4,41 @@ defmodule ConciergeSite.PasswordController do
   alias AlertProcessor.{Model, Repo}
   alias Model.User
 
+  plug :reauthorize_user when action in [:update]
+
   def edit(conn, _params, user, _claims) do
-    changeset = User.update_password_changeset(%User{})
+    changeset = User.update_password_changeset(user)
     render conn, "edit.html", changeset: changeset, user: user
   end
 
   def update(conn, %{"user" => user_params}, user, _claims) do
-    if User.check_password(user, user_params["current_password"]) do
-      changeset = User.update_password_changeset(
-        user, Map.take(user_params, ["password", "password_confirmation"])
-      )
+    changeset = User.update_password_changeset(
+      user, Map.take(user_params, ["password", "password_confirmation"])
+    )
 
-      case Repo.update(changeset) do
-        {:ok, _user} ->
-          conn
-          |> put_flash(:info, "Your password has been updated.")
-          |> redirect(to: my_account_path(conn, :edit))
-        {:error, changeset} ->
-          render conn,"edit.html", user: user, changeset: changeset
-      end
-    else
+    case Repo.update(changeset) do
+      {:ok, _user} ->
+        conn
+        |> put_flash(:info, "Your password has been updated.")
+        |> redirect(to: my_account_path(conn, :edit))
+      {:error, changeset} ->
+        render conn,"edit.html", user: user, changeset: changeset
+    end
+  end
+
+  defp reauthorize_user(conn, _params) do
+    user = conn.private.guardian_default_resource
+    password = conn.params["user"]["current_password"]
+
+    if !User.check_password(user, password) do
+      changeset = User.update_password_changeset(user)
+
       conn
       |> put_flash(:error, "Current password is incorrect.")
-      |> render("edit.html", %{})
+      |> render("edit.html", changeset: changeset, user: user)
+      |> halt()
+    else
+      conn
     end
   end
 end
