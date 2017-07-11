78764a47da187f2f7da85637810e43e07826d1e6
Brian Fauble <bfauble@intrepid.io>
2017-07-11 14:33:49 -0400

    add helper to make sure form to create subscriptions is only submitted once

--- apps/concierge_site/assets/js/form-helpers.js
+++ apps/concierge_site/assets/js/form-helpers.js
@@ -4,4 +4,11 @@ export default function($) {
   $("input").focus(function(event){
     $(event.target).toggleClass("dirty", true);
   });
+
+  $("form.trip-prefs-form").one("submit", function(event){
+    event.preventDefault();
+    $("form.trip-prefs-form button").prop("disabled", true);
+    $("form.trip-prefs-form input[type=submit]").prop("disabled", true);
+    event.target.submit();
+  });
 }
