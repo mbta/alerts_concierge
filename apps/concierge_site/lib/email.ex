2495025662aae40f20234653351b1835ddbe38e0
Danny Rosenblatt <dgrosenblatt@gmail.com>
2017-07-13 09:33:03 -0400

    Add Password Reset email

---
+++ apps/concierge_site/lib/email.ex
@@ -0,0 +1,20 @@
+defmodule ConciergeSite.Email do
+  use Bamboo.Phoenix, view: ConciergeSite.EmailView
+
+  @from Application.get_env(:concierge_site, __MODULE__)[:from]
+
+  def password_reset_text_email({email, password_reset_id}) do
+    new_email()
+    |> to(email)
+    |> from(@from)
+    |> subject("Reset Your MBTA Alerts Password")
+    |> render("password_reset.text", password_reset_id: password_reset_id)
+  end
+
+  def password_reset_html_email(email, password_reset_id) do
+    {email, password_reset_id}
+    |> password_reset_text_email()
+    |> put_html_layout({ConciergeSite.LayoutView, "email.html"})
+    |> render("password_reset.html", password_reset_id: password_reset_id)
+  end
+end
---
+++ apps/concierge_site/lib/password_reset/password_reset_mailer.ex
@@ -0,0 +1,3 @@
+defmodule ConciergeSite.PasswordResetMailer do
+  use Bamboo.Mailer, otp_app: :concierge_site
+end
