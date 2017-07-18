576151dbb50f75d69e94f54ec86d18e0c9a54061
Danny Rosenblatt <dgrosenblatt@gmail.com>
2017-07-18 07:23:02 -0400

    Use path helpers in password reset controller test

--- apps/concierge_site/test/web/controllers/password_reset_controller_test.exs
+++ apps/concierge_site/test/web/controllers/password_reset_controller_test.exs
@@ -7,14 +7,14 @@ defmodule ConciergeSite.PasswordResetControllerTest do
   alias ConciergeSite.Email
 
   test "GET /reset-password/new", %{conn: conn}  do
-    conn = get(conn, "/reset-password/new")
+    conn = get(conn, password_reset_path(conn, :new))
     assert html_response(conn, 200) =~ "Forget your password?"
   end
 
-  test "GET /reset-password/new with an email associated with a user", %{conn: conn}  do
+  test "POST /reset-password/ with an email associated with a user", %{conn: conn}  do
     user = insert(:user)
     params = %{"password_reset" => %{"email" => user.email}}
-    conn = post(conn, "/reset-password", params)
+    conn = post(conn, password_reset_path(conn, :create), params)
 
     password_reset_count = Repo.one(from p in PasswordReset, select: count("*"))
     password_reset = Repo.one(PasswordReset)
@@ -24,10 +24,10 @@ defmodule ConciergeSite.PasswordResetControllerTest do
     assert_delivered_email Email.password_reset_html_email(user.email, password_reset.id)
   end
 
-  test "GET /reset-password/new with a valid but unknown email", %{conn: conn}  do
+  test "POST /reset-password/ with a valid but unknown email", %{conn: conn}  do
     email = "test@example.com"
     params = %{"password_reset" => %{"email" => email}}
-    conn = post(conn, "/reset-password", params)
+    conn = post(conn, password_reset_path(conn, :create), params)
 
     password_reset_count = Repo.one(from p in PasswordReset, select: count("*"))
 
@@ -36,9 +36,9 @@ defmodule ConciergeSite.PasswordResetControllerTest do
     assert_delivered_email Email.unknown_password_reset_html_email(email)
   end
 
-  test "GET /reset-password/new with an invalid email", %{conn: conn}  do
+  test "POST /reset-password/ with an invalid email", %{conn: conn}  do
     params = %{"password_reset" => %{"email" => "blerg"}}
-    conn = post(conn, "/reset-password", params)
+    conn = post(conn, password_reset_path(conn, :create), params)
 
     password_reset_count = Repo.one(from p in PasswordReset, select: count("*"))
 
