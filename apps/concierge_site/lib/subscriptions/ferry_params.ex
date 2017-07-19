23db7c2db53b001beafcf4268972d38c35ec24bc
Brian Fauble <bfauble@intrepid.io>
2017-07-19 12:29:04 -0400

    extract trip_schedule_info_map functions to respective mapper modules in alert_processor app

--- apps/concierge_site/lib/subscriptions/ferry_params.ex
+++ apps/concierge_site/lib/subscriptions/ferry_params.ex
@@ -4,7 +4,7 @@ defmodule ConciergeSite.Subscriptions.FerryParams do
   """
 
   import ConciergeSite.Subscriptions.ParamsValidator
-  alias AlertProcessor.{ApiClient, Helpers.DateTimeHelper, ServiceInfoCache, Subscription.FerryMapper}
+  alias AlertProcessor.Subscription.FerryMapper
 
   @spec validate_info_params(map) :: :ok | {:error, String.t}
   def validate_info_params(params) do
@@ -95,7 +95,7 @@ defmodule ConciergeSite.Subscriptions.FerryParams do
   """
   @spec prepare_for_mapper(map) :: map
   def prepare_for_mapper(%{"relevant_days" => relevant_days, "origin" => origin, "destination" => destination, "trips" => trips, "return_trips" => return_trips, "trip_type" => "round_trip"} = params) do
-    trip_schedule_map = trip_schedule_info_map(origin, destination, relevant_days)
+    trip_schedule_map = FerryMapper.trip_schedule_info_map(origin, destination, relevant_days)
     {departure_start, departure_end} = subscription_timestamps(trip_schedule_map, origin, destination, trips)
     {return_start, return_end} = subscription_timestamps(trip_schedule_map, destination, origin, return_trips)
 
@@ -109,7 +109,7 @@ defmodule ConciergeSite.Subscriptions.FerryParams do
     })
   end
   def prepare_for_mapper(%{"relevant_days" => relevant_days, "origin" => origin, "destination" => destination, "trips" => trips, "trip_type" => "one_way"} = params) do
-    trip_schedule_map = trip_schedule_info_map(origin, destination, relevant_days)
+    trip_schedule_map = FerryMapper.trip_schedule_info_map(origin, destination, relevant_days)
     {departure_start, departure_end} = subscription_timestamps(trip_schedule_map, origin, destination, trips)
 
     Map.merge(params, %{
@@ -120,50 +120,6 @@ defmodule ConciergeSite.Subscriptions.FerryParams do
       "return_start" => nil,
       "return_end" => nil
     })
-  end
-
-  defp trip_schedule_info_map(origin, destination, relevant_days) do
-    relevant_date = DateTimeHelper.determine_date(relevant_days, Date.utc_today())
-    {:ok, data, includes} = ApiClient.schedules(origin, destination, nil, [], relevant_date)
-    includes_info =
-      for include <- includes, into: %{} do
-        case include do
-          %{"type" => "trip", "id" => id} ->
-            {:ok, generalized_trip_id} = ServiceInfoCache.get_generalized_trip_id(id)
-            {id, generalized_trip_id}
-          %{"type" => "stop", "relationships" => %{"parent_station" => %{"data" => parent_station}}, "id" => id} ->
-            case {id, parent_station} do
-              {^origin, nil} ->
-                {origin, origin}
-              {^destination, nil} ->
-                {destination, destination}
-              {id, %{"id" => parent_station_id}} ->
-                {id, parent_station_id}
-            end
-        end
-      end
-
-    for schedule <- data, into: %{} do
-      %{
-        "attributes" => %{
-          "departure_time" => departure_time
-        },
-        "relationships" => %{
-          "trip" => %{
-            "data" => %{
-              "id" => trip_id
-            }
-          },
-          "stop" => %{
-            "data" => %{
-              "id" => stop_id
-            }
-          }
-        }
-      } = schedule
-
-      {{includes_info[stop_id], includes_info[trip_id]}, departure_time}
-    end
   end
 
   defp subscription_timestamps(trip_schedule_map, origin, destination, trips) do
@@ -179,7 +135,6 @@ defmodule ConciergeSite.Subscriptions.FerryParams do
     trips
     |> Enum.map(& schedule_map[{station, &1}])
     |> sort_order.()
-    |> NaiveDateTime.from_iso8601!()
     |> Calendar.Strftime.strftime!("%H:%M:%S")
   end
 end
