2d2444f2ffb7feaa17730a18b7e8da0ecac85dea
faizaanshamsi <dev@faizaanshamsi.com>
2017-07-20 15:39:56 -0400

    redirect / to /my-subscriptions

--- apps/concierge_site/lib/controllers/page_controller.ex
+++ apps/concierge_site/lib/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule ConciergeSite.PageController do
   use ConciergeSite.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    redirect(conn, to: subscription_path(conn, :index))
   end
 end
