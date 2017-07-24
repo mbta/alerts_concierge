fad70c21aeb633c06b601bbe573b687054172be2
Danny Rosenblatt <dgrosenblatt@gmail.com>
2017-07-24 15:26:48 -0400

    Increment version to 0.0.18

--- apps/concierge_site/mix.exs
+++ apps/concierge_site/mix.exs
@@ -3,7 +3,7 @@ defmodule ConciergeSite.Mixfile do
 
   def project do
     [app: :concierge_site,
-     version: "0.0.17",
+     version: "0.0.18",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
