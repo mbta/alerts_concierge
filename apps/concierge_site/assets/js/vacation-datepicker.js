8500a3fda2267b5b122f429b73fba7a6ad1c4193
Danny Rosenblatt <dgrosenblatt@gmail.com>
2017-07-25 09:47:19 -0400

    Set default vacation of now to one week from now

--- apps/concierge_site/assets/js/vacation-datepicker.js
+++ apps/concierge_site/assets/js/vacation-datepicker.js
@@ -1,13 +1,25 @@
 const flatpickr = require("flatpickr");
+const moment = require("moment");
 
 export default function($) {
   $ = $ || window.jQuery;
 
-  const flatpickrConfig = {
-    dateFormat: "Y-m-dT00:00:00+00:00",
-    altInput: true
+  const flatpickrBaseConfig = {
+    altInput: true,
+    dateFormat: "Y-m-dT00:00:00+00:00"
   };
 
-  flatpickr("#user_vacation_start", flatpickrConfig);
-  flatpickr("#user_vacation_end", flatpickrConfig);
+  const now = moment().format();
+  const vacationStartConfig = Object.assign({}, flatpickrBaseConfig, {
+    defaultDate: now
+  });
+
+  const vacationEndConfig = Object.assign({}, flatpickrBaseConfig, {
+    defaultDate: moment().add(7, "days").format(),
+    minDate: now
+  });
+
+  flatpickr("#user_vacation_start", vacationStartConfig);
+  flatpickr("#user_vacation_end", vacationEndConfig);
+
 }
