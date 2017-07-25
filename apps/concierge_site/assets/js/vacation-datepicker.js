7edffd2e26a0b3da6cbd09d0001777892e2e553c
Danny Rosenblatt <dgrosenblatt@gmail.com>
2017-07-25 09:27:19 -0400

    Add separate date pickers for start and end of vacation period

--- apps/concierge_site/assets/js/vacation-datepicker.js
+++ apps/concierge_site/assets/js/vacation-datepicker.js
@@ -3,31 +3,11 @@ const flatpickr = require("flatpickr");
 export default function($) {
   $ = $ || window.jQuery;
 
-  const $vacationStartInput = $("#user_vacation_start");
-  const $vacationEndInput = $("#user_vacation_end");
-  
-  $vacationStartInput.after("<div class='vacation-start-display'></div>");
-  $vacationStartInput.css("display", "none");
-  $vacationEndInput.after("<div class='vacation-end-display'></div>");
-  $vacationEndInput.css("display", "none");
-
-  const setVacationDates = function(selectedDates, dateStr, instance) {
-    $vacationStartInput.val(selectedDates[0].toISOString());
-    $(".vacation-start-display").text(selectedDates[0].toLocaleDateString());
-    if (selectedDates[1]) {
-      $vacationEndInput.val(selectedDates[1].toISOString());
-      $(".vacation-end-display").text(selectedDates[1].toLocaleDateString());
-    }
-  }
-
   const flatpickrConfig = {
     dateFormat: "Y-m-dT00:00:00+00:00",
-    mode: "range",
-    onChange: setVacationDates
+    altInput: true
   };
-  const datePicker = flatpickr(".vacation-datepicker", flatpickrConfig);
 
-  $(document).on("click", ".vacation-date-form-section", function() {
-    datePicker.open();
-  });
+  flatpickr("#user_vacation_start", flatpickrConfig);
+  flatpickr("#user_vacation_end", flatpickrConfig);
 }
