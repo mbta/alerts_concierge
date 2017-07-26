6b92730d0938deb99f441922bd33a79908ce8626
Danny Rosenblatt <dgrosenblatt@gmail.com>
2017-07-26 09:53:34 -0400

    Update double click disabling css class name

--- apps/concierge_site/assets/js/form-helpers.js
+++ apps/concierge_site/assets/js/form-helpers.js
@@ -5,10 +5,10 @@ export default function($) {
     $(event.target).toggleClass("dirty", true);
   });
 
-  $("form.sub-creation-form").one("submit", function(event){
+  $("form.single-submit-form").one("submit", function(event){
     event.preventDefault();
-    $("form.sub-creation-form button").prop("disabled", true);
-    $("form.sub-creation-form input[type=submit]").prop("disabled", true);
+    $("form.single-submit-form button").prop("disabled", true);
+    $("form.single-submit-form input[type=submit]").prop("disabled", true);
     event.target.submit();
   });
 }
