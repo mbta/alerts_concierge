7e836e11f066d6bf65eebaffc95947a50d849b79
Brian Fauble <bfauble@intrepid.io>
2017-07-26 13:07:11 -0400

    use update instead of update all for putting user into indefinite vacation mode

--- apps/concierge_site/lib/controllers/unsubscribe_controller.ex
+++ apps/concierge_site/lib/controllers/unsubscribe_controller.ex
@@ -9,7 +9,7 @@ defmodule ConciergeSite.UnsubscribeController do
       true <- Guardian.Permissions.any?(default_permissions, [:unsubscribe], :default),
       {:ok, user} <- Guardian.serializer.from_token(claims["sub"]),
       :ok <- User.clear_holding_queue_for_user_id(user.id),
-      {1, nil} <- User.put_users_on_indefinite_vacation([user.id]) do
+      {:ok, _user} <- User.put_user_on_indefinite_vacation(user) do
       render(conn, "unsubscribe.html")
     else
       _ ->
