58e478287b17330a06249d9eb16d956161d44c78
faizaanshamsi <dev@faizaanshamsi.com>
2017-08-01 16:19:10 -0400

    Migration Task

---
+++ apps/alert_processor/lib/release_tasks.ex
@@ -0,0 +1,20 @@
+defmodule AlertProcessor.ReleaseTasks do
+  @moduledoc "Tasks that need to be available in a release"
+
+  alias AlertProcessor.Repo
+  alias Ecto.Migrator
+
+  @start_apps [
+    :crypto,
+    :ssl,
+    :postgrex,
+    :ecto
+  ]
+
+  def migrate! do
+    Enum.each(@start_apps, &Application.ensure_all_started/1)
+    Migrator.run(Repo, migrations_path(:alert_processor), :up, all: true)
+  end
+
+  defp migrations_path(app), do: Application.app_dir(app, "priv/repo/migrations")
+end
