4dfba12c0fa469e9f12741d8a2b74a12c23f7c0f
Ricky Chen <richard.polin.chen@gmail.com>
2017-08-02 11:28:53 -0400

    Add test to ensure that digest emails do not have duplicated alerts

--- apps/alert_processor/test/alert_processor/digest/digest_builder_test.exs
+++ apps/alert_processor/test/alert_processor/digest/digest_builder_test.exs
@@ -75,4 +75,25 @@ defmodule AlertProcessor.DigestBuilderTest do
     digests = DigestBuilder.build_digests({[@alert2], @ddg})
     assert digests == [%Digest{user: user, alerts: [@alert2], digest_date_group: @ddg}]
   end
+
+  test "build_digest/1 does not return duplicate alerts for each user based on informed entity" do
+    user1 = insert(:user)
+    user2 = insert(:user)
+    sub1 = insert(:subscription, user: user1)
+    sub2 = insert(:subscription, user: user2)
+    InformedEntity
+    |> struct(@ie1)
+    |> Map.merge(%{subscription_id: sub1.id})
+    |> insert
+    InformedEntity
+    |> struct(@ie2)
+    |> Map.merge(%{subscription_id: sub2.id})
+    |> insert
+
+    digests = DigestBuilder.build_digests({[@alert1, @alert1, @alert2], @ddg})
+    expected = [%Digest{user: user1, alerts: [@alert1], digest_date_group: @ddg},
+                %Digest{user: user2, alerts: [@alert2, @alert1], digest_date_group: @ddg}]
+
+    assert digests == expected
+  end
 end
