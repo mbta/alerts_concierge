5d3f5eeed7054a65dc3cf6c6122ac917f63d3629
Ricky Chen <richard.polin.chen@gmail.com>
2017-08-03 16:47:50 -0400

    Add tests for adding admin users

--- apps/concierge_site/test/web/controllers/admin/admin_user_controller_test.exs
+++ apps/concierge_site/test/web/controllers/admin/admin_user_controller_test.exs
@@ -47,4 +47,52 @@ defmodule ConciergeSite.Admin.AdminUserControllerTest do
       assert html_response(conn, 302) =~ "/login/new"
     end
   end
+
+  describe "valid params" do
+    test "admin user can create other admin users", %{conn: conn} do
+      conn =
+        :user
+        |> insert(role: "application_administration")
+        |> guardian_login(conn, :token, %{
+            default: Guardian.Permissions.max,
+            admin: [:application_administration, :customer_support]
+          })
+
+      admin_user_params =
+              %{"user" => %{
+                "email" => "admin@mbta.com",
+                "password" => "password1",
+                "password_confirmation" => "password1",
+                "role" => "application_administration"
+              }}
+
+      conn = post(conn, "/admin/admin_users/", admin_user_params)
+      assert html_response(conn, 302) =~ "/admin/admin_users"
+    end
+  end
+
+  describe "invalid params" do
+    test "admin user cannot create other admin users without valid params", %{conn: conn} do
+      conn =
+        :user
+        |> insert(role: "application_administration")
+        |> guardian_login(conn, :token, %{
+            default: Guardian.Permissions.max,
+            admin: [:application_administration, :customer_support]
+          })
+
+      invalid_admin_user_params =
+              %{"user" => %{
+                "email" => "admin@mbta.com",
+                "password" => "password1",
+                "password_confirmation" => "password1",
+                "role" => nil
+              }}
+
+      conn = post(conn, "/admin/admin_users/", invalid_admin_user_params)
+      response = html_response(conn, 200)
+
+      assert response =~ "can&#39;t be blank"
+    end
+  end
 end
