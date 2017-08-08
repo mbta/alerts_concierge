59d34f791e2c98afd7d076d61fea0893d31998c0
Danny Rosenblatt <dgrosenblatt@gmail.com>
2017-08-08 15:07:05 -0400

    Add endpoint for updating admin user

--- apps/concierge_site/lib/controllers/admin/my_account_controller.ex
+++ apps/concierge_site/lib/controllers/admin/my_account_controller.ex
@@ -1,8 +1,28 @@
 defmodule ConciergeSite.Admin.MyAccountController do
   use ConciergeSite.Web, :controller
   use Guardian.Phoenix.Controller
+  alias AlertProcessor.Model.User
+  alias ConciergeSite.UserParams
 
   def edit(conn, _params, user, _claims) do
-    render conn, "edit.html", user: user
+    changeset = User.update_account_changeset(user)
+    render conn, "edit.html", user: user, changeset: changeset
+  end
+
+  def update(conn, %{"user" => user_params}, user, _claims) do
+    update_params = UserParams.prepare_for_update_changeset(user_params)
+
+    case User.update_account(user, update_params) do
+      {:ok, _user} ->
+        conn
+        |> put_flash(:info, "Account updated.")
+        |> redirect(to: admin_my_account_path(conn, :edit))
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, "Account could not be updated. Please see errors below.")
+        |> assign(:user, user)
+        |> assign(:changeset, changeset)
+        |> render("edit.html")
+    end
   end
 end
--- apps/concierge_site/lib/router.ex
+++ apps/concierge_site/lib/router.ex
@@ -99,7 +99,7 @@ defmodule ConciergeSite.Router do
     patch "/admin_users/:id/deactivate", Admin.AdminUserController, :deactivate
     patch "/admin_users/:id/activate", Admin.AdminUserController, :activate
     resources "/admin_users", Admin.AdminUserController, only: [:index, :show, :new, :create]
-    resources "/my-account", Admin.MyAccountController, only: [:edit], singleton: true
+    resources "/my-account", Admin.MyAccountController, only: [:edit, :update], singleton: true
   end
 
   if Mix.env == :dev do
