3a9ab2886bb382da02178a678652b8ae914e098b
Ricky Chen <richard.polin.chen@gmail.com>
2017-08-09 16:23:10 -0400

    Styling active links on admin nav bar

--- apps/concierge_site/lib/views/layout_view.ex
+++ apps/concierge_site/lib/views/layout_view.ex
@@ -4,4 +4,11 @@ defmodule ConciergeSite.LayoutView do
   def admin_user(conn), do: Guardian.Plug.current_resource(conn)
   def admin_user?(conn), do: admin_user(conn).role in ~w(application_administration customer_support)
   def admin_logged_in?(conn), do: Guardian.Plug.authenticated?(conn) && admin_user?(conn)
+
+  def active_nav_class(conn, page_name) do
+    [_, endpoint] = conn.path_info
+    if endpoint == page_name do
+      "nav-active"
+    end
+  end
 end
