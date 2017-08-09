b1ffc7df67a25c4e1df006c7e44a26bac1fc292e
Ricky Chen <richard.polin.chen@gmail.com>
2017-08-09 16:06:56 -0400

    Add admin navbar

--- apps/concierge_site/lib/views/layout_view.ex
+++ apps/concierge_site/lib/views/layout_view.ex
@@ -1,3 +1,7 @@
 defmodule ConciergeSite.LayoutView do
   use ConciergeSite.Web, :view
+
+  def admin_user(conn), do: Guardian.Plug.current_resource(conn)
+  def admin_user?(conn), do: admin_user(conn).role in ~w(application_administration customer_support)
+  def admin_logged_in?(conn), do: Guardian.Plug.authenticated?(conn) && admin_user?(conn)
 end
