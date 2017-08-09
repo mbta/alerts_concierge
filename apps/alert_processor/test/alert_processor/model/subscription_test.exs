860e5197a14a05a59f79ba2faa46ee4155e7d405
faizaanshamsi <dev@faizaanshamsi.com>
2017-08-09 16:13:39 -0400

    Admin can search all versions of a subscription by date

--- apps/alert_processor/test/alert_processor/model/subscription_test.exs
+++ apps/alert_processor/test/alert_processor/model/subscription_test.exs
@@ -335,4 +335,43 @@ defmodule AlertProcessor.Model.SubscriptionTest do
       assert nil == Repo.get(Subscription, subscription.id)
     end
   end
+
+  describe "get_versions_by_date/2" do
+    test "returns latest version that existed on the given date" do
+      subscriber = insert(:user)
+      {:ok, future_date} = NaiveDateTime.from_iso8601("2118-01-01T01:01:01")
+      {:ok, date} = NaiveDateTime.from_iso8601("2017-07-11T01:01:01")
+      {:ok, past_date} = NaiveDateTime.from_iso8601("2017-01-11T01:01:01")
+      sub_params = params_for(
+        :subscription,
+        user: subscriber,
+        updated_at: past_date,
+        inserted_at: past_date,
+        relevant_days: [:sunday]
+      )
+      create_changeset = Subscription.create_changeset(%Subscription{}, sub_params)
+      inserted_sub = PaperTrail.insert!(create_changeset)
+      {:ok, updated_sub} = Subscription.update_subscription(inserted_sub, %{
+        updated_at: date
+      })
+      {:ok, future_updated_sub} = Subscription.update_subscription(updated_sub, %{
+        updated_at: future_date
+      })
+
+      # update papertrail version dates
+      [original_version, updated_version, future_version] = PaperTrail.get_versions(future_updated_sub)
+      insert_changeset = Ecto.Changeset.cast(original_version, %{inserted_at: past_date}, [:inserted_at])
+      update_changeset = Ecto.Changeset.cast(updated_version, %{inserted_at: date}, [:inserted_at])
+      future_changeset = Ecto.Changeset.cast(future_version, %{inserted_at: future_date}, [:inserted_at])
+
+      Repo.update!(insert_changeset)
+      Repo.update!(update_changeset)
+      Repo.update!(future_changeset)
+
+      [original, updated] = Subscription.get_versions_by_date(subscriber, date)
+
+      assert original.id == original_version.id
+      assert updated.id == updated_version.id
+    end
+  end
 end
