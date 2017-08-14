dc434046e222ae899658345c93f3c6a99035353c
Brian Fauble <bfauble@intrepid.io>
2017-08-14 09:37:10 -0400

    remove inspect and use phoenix.html.safe_to_string() instead of matching on {:safe, _} tuple

--- apps/concierge_site/test/web/views/pagination_helpers_test.exs
+++ apps/concierge_site/test/web/views/pagination_helpers_test.exs
@@ -6,37 +6,37 @@ defmodule ConciergeSite.PaginationHelpersTest do
     test "only one page" do
       conn = build_conn(:get, "/test_path")
       page = %Scrivener.Page{page_number: 1, total_pages: 1, entries: [1, 2, 3]}
-      {:safe, pagination} = PaginationHelpers.paginate(conn, page)
-      assert IO.iodata_to_binary(pagination) =~ "1 of 1"
-      refute IO.iodata_to_binary(pagination) =~ "?page=0"
-      refute IO.iodata_to_binary(pagination) =~ "?page=2"
+      pagination = conn |> PaginationHelpers.paginate(page) |> Phoenix.HTML.safe_to_string()
+      assert pagination =~ "1 of 1"
+      refute pagination =~ "?page=0"
+      refute pagination =~ "?page=2"
     end
 
     test "first of multiple pages" do
       conn = build_conn(:get, "/test_path", %{"page" => "1"})
       page = %Scrivener.Page{page_number: 1, total_pages: 3, entries: [1, 2, 3]}
-      {:safe, pagination} = PaginationHelpers.paginate(conn, page)
-      assert IO.iodata_to_binary(pagination) =~ "1 of 3"
-      assert IO.iodata_to_binary(pagination) =~ "?page=2"
-      refute IO.iodata_to_binary(pagination) =~ "?page=0"
+      pagination = conn |> PaginationHelpers.paginate(page) |> Phoenix.HTML.safe_to_string()
+      assert pagination =~ "1 of 3"
+      assert pagination =~ "?page=2"
+      refute pagination =~ "?page=0"
     end
 
     test "middle of multiple pages" do
       conn = build_conn(:get, "/test_path", %{"page" => "2"})
       page = %Scrivener.Page{page_number: 2, total_pages: 3, entries: [1, 2, 3]}
-      {:safe, pagination} = PaginationHelpers.paginate(conn, page)
-      assert IO.iodata_to_binary(pagination) =~ "2 of 3"
-      assert IO.iodata_to_binary(pagination) =~ "?page=1"
-      assert IO.iodata_to_binary(pagination) =~ "?page=3"
+      pagination = conn |> PaginationHelpers.paginate(page) |> Phoenix.HTML.safe_to_string()
+      assert pagination =~ "2 of 3"
+      assert pagination =~ "?page=1"
+      assert pagination =~ "?page=3"
     end
 
     test "last of multiple pages" do
       conn = build_conn(:get, "/test_path", %{"page" => "3"})
       page = %Scrivener.Page{page_number: 3, total_pages: 3, entries: [1, 2, 3]}
-      {:safe, pagination} = PaginationHelpers.paginate(conn, page)
-      assert IO.iodata_to_binary(pagination) =~ "3 of 3"
-      assert IO.iodata_to_binary(pagination) =~ "?page=2"
-      refute IO.iodata_to_binary(pagination) =~ "?page=4"
+      pagination = conn |> PaginationHelpers.paginate(page) |> Phoenix.HTML.safe_to_string()
+      assert pagination =~ "3 of 3"
+      assert pagination =~ "?page=2"
+      refute pagination =~ "?page=4"
     end
   end
 end
