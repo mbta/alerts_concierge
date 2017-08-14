9bba522efd2f3000d5fa971342bcc61f0e66dd7c
Ricky Chen <richard.polin.chen@gmail.com>
2017-08-14 10:45:49 -0400

    Version 0.0.26

--- apps/alert_processor/mix.exs
+++ apps/alert_processor/mix.exs
@@ -3,7 +3,7 @@ defmodule AlertProcessor.Mixfile do
 
   def project do
     [app: :alert_processor,
-     version: "0.0.25",
+     version: "0.0.26",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
