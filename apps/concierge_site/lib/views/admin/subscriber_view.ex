c43687cb4e99c57fb5f577d0f4c89c33bed45d85
Brian Fauble <bfauble@intrepid.io>
2017-08-15 15:24:55 -0400

    add basic subscriber details page

--- apps/concierge_site/lib/views/admin/subscriber_view.ex
+++ apps/concierge_site/lib/views/admin/subscriber_view.ex
@@ -2,6 +2,7 @@ defmodule ConciergeSite.Admin.SubscriberView do
   use ConciergeSite.Web, :view
 
   alias AlertProcessor.Model.User
+  alias Calendar.Strftime
 
   @spec account_status(User.t) :: String.t
   def account_status(%User{encrypted_password: ""}), do: "Disabled"
@@ -10,5 +11,27 @@ defmodule ConciergeSite.Admin.SubscriberView do
 
   def email_is_default?(user) do
     is_nil(user.phone_number)
+  end
+
+  def blackout_period(%User{do_not_disturb_start: nil, do_not_disturb_end: nil}), do: "N/A"
+  def blackout_period(%User{do_not_disturb_start: dnd_start, do_not_disturb_end: dnd_end}) do
+    [
+      Strftime.strftime!(dnd_start, "%I:%M %p"),
+      " to ",
+      Strftime.strftime!(dnd_end, "%I:%M %p")
+    ]
+  end
+
+  def vacation_period(%User{vacation_start: nil, vacation_end: nil}), do: "N/A"
+  def vacation_period(%User{vacation_start: vacation_start, vacation_end: vacation_end}) do
+    if DateTime.compare(vacation_end, DateTime.utc_now()) == :lt do
+      "N/A"
+    else
+      [
+        Strftime.strftime!(vacation_start, "%c"),
+        " until ",
+        Strftime.strftime!(vacation_end, "%c")
+      ]
+    end
   end
 end
