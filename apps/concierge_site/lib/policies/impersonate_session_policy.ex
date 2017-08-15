c22acfd9fa393d191910d58978415ba6f50adce8
Danny Rosenblatt <dgrosenblatt@gmail.com>
2017-08-15 14:49:17 -0400

    Remove unreachable function

--- apps/concierge_site/lib/policies/impersonate_session_policy.ex
+++ apps/concierge_site/lib/policies/impersonate_session_policy.ex
@@ -4,6 +4,4 @@ defmodule ConciergeSite.ImpersonateSessionPolicy do
   def can?(admin, :impersonate_user, user) do
     User.is_admin?(admin) and not User.is_admin?(user)
   end
-  
-  def can?(%User{}, :impersonate_user, %User{}), do: false
 end
