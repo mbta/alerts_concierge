ca3b01b60ecd71105e36e009473204c837e50651
Danny Rosenblatt <dgrosenblatt@gmail.com>
2017-08-15 13:52:53 -0400

    Use `User.is_admin?/1` in ImpersonateSessionPolicy instead of module attribute

--- apps/concierge_site/lib/policies/impersonate_session_policy.ex
+++ apps/concierge_site/lib/policies/impersonate_session_policy.ex
@@ -1,8 +1,9 @@
 defmodule ConciergeSite.ImpersonateSessionPolicy do
   alias AlertProcessor.Model.User
 
-  @admin_roles ~w(application_administration customer_support)
-
-  def can?(%User{role: admin_role}, :impersonate_user, %User{role: "user"}) when admin_role in @admin_roles, do: true
+  def can?(admin, :impersonate_user, user) do
+    User.is_admin?(admin) and not User.is_admin?(user)
+  end
+  
   def can?(%User{}, :impersonate_user, %User{}), do: false
 end
