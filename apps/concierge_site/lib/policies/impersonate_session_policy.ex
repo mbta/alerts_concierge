04eb26855f4911e75acf7ff80b0c103ecc4b31a9
Danny Rosenblatt <dgrosenblatt@gmail.com>
2017-08-15 09:32:28 -0400

    Style fix

--- apps/concierge_site/lib/policies/impersonate_session_policy.ex
+++ apps/concierge_site/lib/policies/impersonate_session_policy.ex
@@ -3,6 +3,6 @@ defmodule ConciergeSite.ImpersonateSessionPolicy do
 
   @admin_roles ~w(application_administration customer_support)
 
-  def can?(%User{role: admin_role}, :impersonate_user, %User{role: "user"})when admin_role in @admin_roles, do: true
+  def can?(%User{role: admin_role}, :impersonate_user, %User{role: "user"}) when admin_role in @admin_roles, do: true
   def can?(%User{}, :impersonate_user, %User{}), do: false
 end
