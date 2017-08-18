b5ce3fc85c636c87a7017f3486e42ef34e4e44f4
Ricky Chen <richard.polin.chen@gmail.com>
2017-08-18 11:19:27 -0400

    Version 0.0.29

--- apps/concierge_site/mix.exs
+++ apps/concierge_site/mix.exs
@@ -3,7 +3,7 @@ defmodule ConciergeSite.Mixfile do
 
   def project do
     [app: :concierge_site,
-     version: "0.0.27",
+     version: "0.0.29",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
