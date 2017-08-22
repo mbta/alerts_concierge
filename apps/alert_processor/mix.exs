eae4d8d09a51788f9134286060470689f459caf1
Ricky Chen <richard.polin.chen@gmail.com>
2017-08-22 13:54:16 -0400

    Version 0.0.30

--- apps/alert_processor/mix.exs
+++ apps/alert_processor/mix.exs
@@ -3,7 +3,7 @@ defmodule AlertProcessor.Mixfile do
 
   def project do
     [app: :alert_processor,
-     version: "0.0.29",
+     version: "0.0.30",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
--- apps/concierge_site/mix.exs
+++ apps/concierge_site/mix.exs
@@ -3,7 +3,7 @@ defmodule ConciergeSite.Mixfile do
 
   def project do
     [app: :concierge_site,
-     version: "0.0.29",
+     version: "0.0.30",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
