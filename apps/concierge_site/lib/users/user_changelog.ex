aea5ccd9d4a6d8d4886837dec3365f99a4b66687
Brian Fauble <bfauble@intrepid.io>
2017-08-23 11:06:52 -0400

    add subscriber account activity log to subscriber show page

---
+++ apps/concierge_site/lib/users/user_changelog.ex
@@ -0,0 +1,169 @@
+defmodule ConciergeSite.UserChangelog do
+  alias AlertProcessor.{Model.Subscription, Model.User, Repo}
+  alias Calendar.Strftime
+  import Ecto.Query
+
+  def changelog(user_id) do
+    account_changes = account_changes_by_user_id(user_id)
+    originating_user_ids =
+      account_changes
+      |> Enum.flat_map(fn(%{originator_id: originator_id}) ->
+           (originator_id && [originator_id]) || []
+         end)
+    originating_user_email_map = Repo.all(from u in User, where: u.id in ^originating_user_ids, select: {u.id, u.email}) |> Map.new
+
+    {changelog, _} =
+      user_id
+      |> account_changes_by_user_id()
+      |> Enum.flat_map_reduce(%{}, fn(change, acc) ->
+           changelog_item(change, acc, originating_user_email_map)
+         end)
+    changelog
+  end
+
+  defp account_changes_by_user_id(user_id) do
+    Repo.all(
+      from v in PaperTrail.Version,
+      left_join: s in Subscription,
+      on: v.item_id == s.id and v.item_type == "Subscription",
+      where: s.user_id == ^user_id,
+      or_where: fragment("?->>'owner' = ?", v.meta, ^user_id),
+      or_where: v.item_id == ^user_id and is_nil(v.origin),
+      order_by: [asc: v.inserted_at]
+    )
+  end
+
+  defp changelog_item(%{
+    inserted_at: inserted_at,
+    event: "insert",
+    originator_id: id,
+    item_changes: item_changes,
+    item_id: item_id,
+    item_type: "Subscription"
+    }, acc, originating_user_email_map) do
+      rest =
+        case item_changes do
+          %{"type" => "amenity"} -> "amenity subscription " <> item_id
+          %{"type" => "subway", "origin" => origin, "destination" => destination} -> "subway subscription " <> item_id <> " between " <> origin <> " and " <> destination
+          %{"type" => "commuter_rail", "origin" => origin, "destination" => destination} -> "commuter_rail subscription " <> item_id <> " between " <> origin <> " and " <> destination
+          %{"type" => "ferry", "origin" => origin, "destination" => destination} -> "ferry subscription " <> item_id <> " between " <> origin <> " and " <> destination
+          %{"type" => "bus"} -> "bus subscription " <> item_id
+        end
+      {[originating_user_email_map[id] <> " created " <> rest <> " on " <> Strftime.strftime!(inserted_at, "%F %T")], Map.put(acc, item_id, item_changes)}
+  end
+  defp changelog_item(%{
+    inserted_at: inserted_at,
+    event: "update",
+    originator_id: id,
+    item_changes: item_changes,
+    item_id: item_id,
+    item_type: "Subscription"
+    }, acc, originating_user_email_map) do
+      old_version = Map.get(acc, item_id)
+      changed_keys = Map.keys(item_changes)
+      message =
+        changed_keys
+        |> Enum.flat_map(fn(changed_key) ->
+            old_value = Map.get(old_version, changed_key) || "N/A"
+            new_value = Map.get(item_changes, changed_key) || "N/A"
+            if old_value == new_value do
+              []
+            else
+              [changed_key <> " from " <> to_string(old_value) <> " to " <> to_string(new_value)]
+            end
+          end)
+        |> Enum.join(", ")
+      {[originating_user_email_map[id] <> " updated " <> message <> " for subscription " <> item_id <> " on " <> Strftime.strftime!(inserted_at, "%F %T")], Map.put(acc, item_id, Map.merge(old_version, item_changes))}
+  end
+  defp changelog_item(%{
+    inserted_at: inserted_at,
+    event: "delete",
+    originator_id: id,
+    item_changes: item_changes,
+    item_id: item_id,
+    item_type: "Subscription"
+    }, acc, originating_user_email_map) do
+      rest =
+        case item_changes do
+          %{"type" => "amenity"} -> "amenity subscription " <> item_id
+          %{"type" => "subway", "origin" => origin, "destination" => destination} -> "subway subscription " <> item_id <> " between " <> origin <> " and " <> destination
+          %{"type" => "commuter_rail", "origin" => origin, "destination" => destination} -> "commuter_rail subscription " <> item_id <> " between " <> origin <> " and " <> destination
+          %{"type" => "ferry", "origin" => origin, "destination" => destination} -> "ferry subscription " <> item_id <> " between" <> origin <> " and " <> destination
+          %{"type" => "bus"} -> "bus subscription " <> item_id
+        end
+      {[originating_user_email_map[id] <> " deleted " <> rest <> " on " <> Strftime.strftime!(inserted_at, "%F %T")], Map.delete(acc, item_id)}
+  end
+  defp changelog_item(%{
+    inserted_at: inserted_at,
+    event: "insert",
+    item_changes: item_changes,
+    item_id: item_id,
+    item_type: "User"
+    }, acc, _) do
+      {["Account created on " <> Strftime.strftime!(inserted_at, "%F %T")], Map.put(acc, item_id, item_changes)}
+  end
+  defp changelog_item(%{
+    inserted_at: inserted_at,
+    event: "update",
+    originator_id: id,
+    item_changes: item_changes,
+    item_id: item_id,
+    item_type: "User"
+    }, acc, originating_user_email_map) do
+      old_version = Map.get(acc, item_id)
+      changed_keys = Map.keys(item_changes)
+      message =
+        changed_keys
+        |> Enum.flat_map(fn(changed_key) ->
+            old_value = Map.get(old_version, changed_key) || "N/A"
+            new_value = Map.get(item_changes, changed_key) || "N/A"
+            if old_value == new_value do
+              []
+            else
+              [changed_key <> " from " <> to_string(old_value) <> " to " <> to_string(new_value)]
+            end
+          end)
+        |> Enum.join(", ")
+      new_state = Map.merge(old_version, item_changes)
+      {[originating_user_email_map[id] <> " updated " <> message <> " on " <> Strftime.strftime!(inserted_at, "%F %T")], Map.put(acc, item_id, new_state)}
+  end
+  defp changelog_item(%{
+    inserted_at: inserted_at,
+    event: "insert",
+    originator_id: id,
+    item_changes: %{"trip" => trip, "subscription_id" => subscription_id},
+    item_type: "InformedEntity"
+    }, acc, originating_user_email_map) when is_binary(trip) do
+      {[originating_user_email_map[id] <> " added trip " <> trip <> " to subscription " <> subscription_id <> " on " <> Strftime.strftime!(inserted_at, "%F %T")], acc}
+  end
+  defp changelog_item(%{
+    inserted_at: inserted_at,
+    event: "insert",
+    originator_id: id,
+    item_changes: %{"stop" => stop, "subscription_id" => subscription_id},
+    item_type: "InformedEntity"
+    }, acc, originating_user_email_map) when is_binary(stop) do
+      {[originating_user_email_map[id] <> " added stop " <> stop <> " to subscription " <> subscription_id <> " on " <> Strftime.strftime!(inserted_at, "%F %T")], acc}
+  end
+  defp changelog_item(%{
+    inserted_at: inserted_at,
+    event: "delete",
+    originator_id: id,
+    item_changes: %{"trip" => trip, "subscription_id" => subscription_id},
+    item_type: "InformedEntity"
+    }, acc, originating_user_email_map) when is_binary(trip) do
+      {[originating_user_email_map[id] <> " removed trip " <> trip <> " from subscription " <> subscription_id <> " on " <> Strftime.strftime!(inserted_at, "%F %T")], acc}
+  end
+  defp changelog_item(%{
+    inserted_at: inserted_at,
+    event: "delete",
+    originator_id: id,
+    item_changes: %{"stop" => stop, "subscription_id" => subscription_id},
+    item_type: "InformedEntity"
+    }, acc, originating_user_email_map) when is_binary(stop) do
+      {[originating_user_email_map[id] <> " removed stop " <> stop <> " from subscription " <> subscription_id <> " on " <> Strftime.strftime!(inserted_at, "%F %T")], acc}
+  end
+  defp changelog_item(%{item_type: "InformedEntity"}, acc, _) do
+    {[], acc}
+  end
+end
---
+++ apps/concierge_site/test/web/users/user_changelog_test.exs
@@ -0,0 +1,75 @@
+defmodule ConciergeSite.UserChangelogTest do
+  use ConciergeSite.DataCase
+
+  alias AlertProcessor.Model.{Subscription, User}
+  alias AlertProcessor.Subscription.CommuterRailMapper
+  alias ConciergeSite.UserChangelog
+
+  setup do
+    {:ok, user} = User.create_account(%{"email" => "test_email@whatever.com", "password" => "Password1", "password_confirmation" => "Password1"})
+    one_way_params = %{
+      "origin" => "Anderson/ Woburn",
+      "destination" => "place-north",
+      "trips" => ["123", "125"],
+      "relevant_days" => ["saturday"],
+      "departure_start" => DateTime.from_naive!(~N[2017-07-20 12:00:00], "Etc/UTC"),
+      "departure_end" => DateTime.from_naive!(~N[2017-07-20 14:00:00], "Etc/UTC"),
+      "return_start" => nil,
+      "return_end" => nil,
+      "alert_priority_type" => "low",
+      "amenities" => ["elevator"]
+    }
+    {:ok, subscription_infos} = CommuterRailMapper.map_subscriptions(one_way_params)
+    multi = CommuterRailMapper.build_subscription_transaction(subscription_infos, user, user.id)
+    Repo.transaction(multi)
+
+    {:ok, user: user}
+  end
+
+  describe "account" do
+    test "maps creation", %{user: user} do
+      [account_creation_log | _] = UserChangelog.changelog(user.id)
+      assert account_creation_log =~ "Account created on "
+    end
+
+    test "maps updating", %{user: user} do
+      User.update_account(user, %{"phone_number" => "5551231234", "do_not_disturb_end" => nil, "do_not_disturb_start" => nil}, user.id)
+      changelog = user.id |> UserChangelog.changelog() |> IO.iodata_to_binary()
+      assert changelog =~ "#{user.email} updated do_not_disturb_end from 07:00:00 to N/A, do_not_disturb_start from 22:00:00 to N/A, phone_number from N/A to 5551231234"
+    end
+  end
+
+  describe "subscription" do
+    test "maps creation", %{user: user} do
+      subscription = Repo.one(from s in Subscription, where: s.user_id == ^user.id)
+      changelog = user.id |> UserChangelog.changelog() |> IO.iodata_to_binary()
+      assert changelog =~ "#{user.email} created commuter_rail subscription #{subscription.id}"
+      assert changelog =~ " between #{subscription.origin} and #{subscription.destination}"
+    end
+
+    test "maps updating", %{user: user} do
+      subscription = Repo.one(from s in Subscription, where: s.user_id == ^user.id, preload: [:informed_entities])
+      params = %{
+        "alert_priority_type" => :high,
+        "start_time" => DateTime.from_naive!(~N[2017-07-20 12:00:00], "Etc/UTC"),
+        "end_time" => DateTime.from_naive!(~N[2017-07-20 15:00:00], "Etc/UTC"),
+        "trips" => ["123", "127"]
+      }
+      multi = CommuterRailMapper.build_update_subscription_transaction(subscription, params, user.id)
+      Repo.transaction(multi)
+      changelog = user.id |> UserChangelog.changelog() |> IO.iodata_to_binary()
+      assert changelog =~ "#{user.email} removed trip 123 from subscription #{subscription.id}"
+      assert changelog =~ "#{user.email} added trip 123 to subscription #{subscription.id}"
+      assert changelog =~ "#{user.email} removed trip 125 from subscription #{subscription.id}"
+      assert changelog =~ "#{user.email} added trip 127 to subscription #{subscription.id}"
+      assert changelog =~ "#{user.email} updated alert_priority_type from low to high, end_time from 14:00:00 to 15:00:00 for subscription #{subscription.id}"
+    end
+
+    test "maps deleting", %{user: user} do
+      subscription = Repo.one(from s in Subscription, where: s.user_id == ^user.id, preload: [:informed_entities])
+      {:ok, _} = Subscription.delete_subscription(subscription, user.id)
+      changelog = user.id |> UserChangelog.changelog() |> IO.iodata_to_binary()
+      assert changelog =~ "#{user.email} deleted commuter_rail subscription #{subscription.id} between #{subscription.origin} and #{subscription.destination}"
+    end
+  end
+end
