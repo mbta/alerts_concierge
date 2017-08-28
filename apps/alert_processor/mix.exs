4286cf4474b0285fb64e282a6906d91dbfa938af
Danny Rosenblatt <dgrosenblatt@gmail.com>
2017-08-28 11:09:12 -0400

    Version 0.0.32

--- apps/alert_processor/mix.exs
+++ apps/alert_processor/mix.exs
@@ -3,7 +3,7 @@ defmodule AlertProcessor.Mixfile do
 
   def project do
     [app: :alert_processor,
-     version: "0.0.31",
+     version: "0.0.32",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
