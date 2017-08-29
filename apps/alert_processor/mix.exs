a835b81a884bd8d2c974f77d7dc73e8bc0511d4e
faizaanshamsi <dev@faizaanshamsi.com>
2017-08-29 15:10:41 -0400

    version bump

--- apps/alert_processor/mix.exs
+++ apps/alert_processor/mix.exs
@@ -3,7 +3,7 @@ defmodule AlertProcessor.Mixfile do
 
   def project do
     [app: :alert_processor,
-     version: "0.0.33",
+     version: "0.0.34",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
