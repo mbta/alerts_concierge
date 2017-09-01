be1a1f0d774e2ca3b2fe7d9f94affdfaa93fbaeb
Brian Fauble <bfauble@intrepid.io>
2017-09-01 11:16:54 -0400

    update active period filter to only pass along subscription once per match and add unique filter to subscription filter engine and update tests

--- apps/alert_processor/test/alert_processor/rules_engine/subscription_filter_engine_test.exs
+++ apps/alert_processor/test/alert_processor/rules_engine/subscription_filter_engine_test.exs
@@ -5,8 +5,9 @@ defmodule AlertProcessor.SubscriptionFilterEngineTest do
 
   setup_all do
     {:ok, start_time} = DateTime.from_naive(~N[2017-04-26 09:00:00], "Etc/UTC")
+    {:ok, start_time2} = DateTime.from_naive(~N[2017-08-01 09:00:00], "Etc/UTC")
     alert = %Alert{
-      active_period: [%{start: start_time, end: nil}],
+      active_period: [%{start: start_time, end: nil}, %{start: start_time2, end: nil}],
       effect_name: "Delay",
       header: "This is a test message",
       id: "1",
@@ -41,7 +42,25 @@ defmodule AlertProcessor.SubscriptionFilterEngineTest do
 
     result = SubscriptionFilterEngine.process_alert(alert, [s1, s2, s3, s4], [])
 
-    assert {:ok, [%Notification{email: email}]} = result
+    assert {:ok, [%Notification{email: email}, %Notification{email: email}]} = result
+    assert email == user.email
+  end
+
+  test "process_alert/1 when multiple matching subscriptions for user only sends 1 notification per active period", %{alert: alert} do
+    user = insert(:user, phone_number: nil)
+
+    s1 = :subscription
+    |> build(user: user, alert_priority_type: :low, informed_entities: [%InformedEntity{route_type: 1}])
+    |> weekday_subscription
+    |> insert
+    s2 = :subscription
+    |> build(user: user, alert_priority_type: :low, informed_entities: [%InformedEntity{route_type: 1}])
+    |> weekday_subscription
+    |> insert
+
+    result = SubscriptionFilterEngine.process_alert(alert, [s1, s2], [])
+
+    assert {:ok, [%Notification{email: email}, %Notification{email: email}]} = result
     assert email == user.email
   end
 end
