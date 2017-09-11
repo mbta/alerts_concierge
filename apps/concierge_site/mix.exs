af22611a0ab96157cdb34696860adc2fc7bffffa
faizaanshamsi <dev@faizaanshamsi.com>
2017-09-11 17:32:50 -0400

    version 39

--- apps/concierge_site/mix.exs
+++ apps/concierge_site/mix.exs
@@ -3,7 +3,7 @@ defmodule ConciergeSite.Mixfile do
 
   def project do
     [app: :concierge_site,
-     version: "0.0.38",
+     version: "0.0.39",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
