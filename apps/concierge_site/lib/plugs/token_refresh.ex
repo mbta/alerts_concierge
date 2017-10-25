8c95b38abf0aefef205ab469f18f6289a30722c7
Brian Fauble <bfauble@intrepid.io>
2017-10-25 09:52:10 -0400

    change refresh timeframe to 15 minutes before expiry up from 5

--- apps/concierge_site/lib/plugs/token_refresh.ex
+++ apps/concierge_site/lib/plugs/token_refresh.ex
@@ -14,8 +14,8 @@ defmodule ConciergeSite.Plugs.TokenRefresh do
   def call(conn, _) do
     with token <- get_session(conn, "guardian_default"),
          {:ok, claims} <- Guardian.decode_and_verify(token),
-         true <- token_expires_within_five_minutes?(claims),
-         {:ok, new_token, new_claims} <- Guardian.refresh!(token, claims, %{ttl: {60, :minutes}}) do
+         true <- token_expires_within_fifteen_minutes?(claims),
+         {:ok, new_token, new_claims} <- Guardian.refresh!(token, claims, %{ttl: {24, :hours}}) do
       current_user = current_resource(conn)
       new_claims = claims_with_permission(claims, new_claims, current_user)
       key = Map.get(new_claims, :key, :default)
@@ -31,9 +31,9 @@ defmodule ConciergeSite.Plugs.TokenRefresh do
     end
   end
 
-  defp token_expires_within_five_minutes?(%{"exp" => exp}) do
+  defp token_expires_within_fifteen_minutes?(%{"exp" => exp}) do
     now = DateTime.utc_now() |> DateTime.to_unix()
-    five_minutes_from_now = 300 + now
-    exp > now and exp < five_minutes_from_now
+    fifteen_minutes_from_now = (15 * 60) + now
+    exp > now and exp < fifteen_minutes_from_now
   end
 end
