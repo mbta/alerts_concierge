ee3765eaaffd831ea971ea482218584bfa7026f1
Ryan Mahoney <ryan@virtuecenter.com>
2017-12-06 13:29:33 -0500

    remove logentries integration

--- apps/alert_processor/config/prod.exs
+++ apps/alert_processor/config/prod.exs
@@ -5,14 +5,7 @@ config :alert_processor, database_url: {:system, "DATABASE_URL_PROD"}
 config :logger,
   level: :info,
   truncate: :infinity,
-  backends: [{Logger.Backend.Logentries, :logentries}, :console]
-
-config :logger, :logentries,
-  connector: Logger.Backend.Logentries.Output.SslKeepOpen,
-  host: 'data.logentries.com',
-  port: 443,
-  token: "${LOGENTRIES_TOKEN}",
-  format: "$dateT$time [$level]$levelpad node=$node $metadata$message\n"
+  backends: [:console]
 
 # Config for ExAws lib
 config :alert_processor, :ex_aws, ExAws
--- apps/concierge_site/config/prod.exs
+++ apps/concierge_site/config/prod.exs
@@ -27,15 +27,7 @@ config :phoenix, :serve_endpoints, true
 config :logger,
   level: :info,
   truncate: :infinity,
-  backends: [{Logger.Backend.Logentries, :logentries}, :console]
-
-config :logger, :logentries,
-  connector: Logger.Backend.Logentries.Output.SslKeepOpen,
-  host: 'data.logentries.com',
-  port: 443,
-  token: "${LOGENTRIES_TOKEN}",
-  format: "$dateT$time [$level]$levelpad node=$node $metadata$message\n",
-  metadata: [:request_id]
+  backends: [:console]
 
 config :concierge_site, ConciergeSite.Dissemination.Mailer,
   adapter: Bamboo.SMTPAdapter,
