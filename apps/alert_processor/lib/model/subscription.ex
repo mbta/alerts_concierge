a6eef9e7f2051059b460617f8d21204887c82e3e
zfletch <zfletch@users.noreply.github.com>
2017-12-22 14:47:29 -0500

    support bus stops with parent stations (#496)
    Sometimes there are elevator delays at bus stops which are part of
    subway stations. In the API this is represented by a parent
    relationship. However, the code that looks for parents wasn't looking at
    busses.
    
    This commit changes the API to look for parent stations for all modes of
    transport, updates the tests, and adds some constants to a list of
    constants that were causing `:erlang.binary_to_existing_atom` to fail.

--- apps/alert_processor/lib/model/subscription.ex
+++ apps/alert_processor/lib/model/subscription.ex
@@ -362,7 +362,7 @@ defmodule AlertProcessor.Model.Subscription do
   for String.to_existing_atom calls.
   """
   def subscription_types do
-    [:bus, :subway, :commuter_rail, :boat, :accessibility, :parking, :bike_storage]
+    [:bus, :subway, :commuter_rail, :boat, :accessibility, :parking, :parking_area, :bike_storage, :elevated_subplatform]
   end
 
   def subscription_type_from_route_type(route_type) do
