b9fc3e4a8a7fbf6435a063f232f334e628cf991f
Ryan Mahoney <ryan@virtuecenter.com>
2018-01-11 11:39:10 -0500

    start :sasl when in prod environment

--- apps/concierge_site/mix.exs
+++ apps/concierge_site/mix.exs
@@ -23,17 +23,24 @@ defmodule ConciergeSite.Mixfile do
   #
   # Type `mix help compile.app` for more information.
   def application do
+    apps = [
+      :elixir_make,
+      :ex2ms,
+      :public_key,
+      :crypto,
+      :bamboo,
+      :logger,
+      :runtime_tools
+    ]
+
+    apps = if Mix.env == :prod do
+      [:sasl | apps]
+    else
+      apps
+    end
+
     [mod: {ConciergeSite, []},
-     extra_applications: [
-       :elixir_make,
-       :ex2ms,
-       :public_key,
-       :crypto,
-       :bamboo,
-       :logger,
-       :runtime_tools
-       ]
-     ]
+     extra_applications: apps]
   end
 
   # Specifies which paths to compile per environment.
