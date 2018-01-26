71b7e037119af029d999584dca096776fd455276
Ryan Mahoney <ryan@virtuecenter.com>
2018-01-25 19:05:03 -0500

    refactor alert matching using a better approach to the alert factory

--- apps/concierge_site/test/integration/matching_test.exs
+++ apps/concierge_site/test/integration/matching_test.exs
@@ -1,40 +1,127 @@
 defmodule ConciergeSite.Integration.Matching do
   use ExUnit.Case
   import ConciergeSite.{SubscriptionFactory, AlertFactory}
-  alias AlertProcessor.{Model.Subscription, Model.InformedEntity, SubscriptionFilterEngine}
+  import AlertProcessor.SubscriptionFilterEngine, only: [determine_recipients: 3]
 
-  @base_params %{"alert_priority_type" => "medium", "departure_end" => ~T[08:30:00],
-                 "departure_start" => ~T[08:00:00], "relevant_days" => ["weekday"],
-                 "return_start" => nil, "return_end" => nil}
+  @base %{"alert_priority_type" => "medium", "departure_end" => ~T[08:30:00], "departure_start" => ~T[08:00:00],
+          "relevant_days" => ["weekday"], "return_start" => nil, "return_end" => nil}
+  @alert_severity severity_by_priority(@base["alert_priority_type"])
+  @alert_active_period [active_period(@base["departure_start"], @base["departure_end"], :monday)]
+  @alert_weekend_period [active_period(@base["departure_start"], @base["departure_end"], :sunday)]
+  @alert_later_period [active_period(~T[09:00:00], ~T[09:30:00], :monday)]
 
+  describe "subway subscription" do
+    @subscription subscription(:subway, Map.merge(@base, %{"destination" => "place-brdwy",
+                                                           "origin" => "place-harsq", "roaming" => "false"}))
+    @subscription_roaming subscription(:subway, Map.merge(@base, %{"destination" => "place-brdwy",
+                                                                   "origin" => "place-harsq", "roaming" => "true"}))
+    @alert_entity %{"route_type" => 1, "route_id" => "Red", "direction_id" => 0, "stop_id" => "place-harsq",
+                   "activities" => ["BOARD"]}
 
-  test "unified interface for creating subscriptions that matches front-end process" do
-    subway_params = Map.merge(@base_params, %{"destination" => "place-chncl", "origin" => "place-ogmnl",
-                                             "roaming" => "false"})
-    assert %Subscription{informed_entities: [%InformedEntity{} | _]} = subscription(:subway, subway_params)
+    # Same
+    test "all same: stop, direction, time, activity and severity" do
+      alert = alert(%{"severity" => @alert_severity, "active_period" => @alert_active_period,
+                      "informed_entity" => [@alert_entity]})
+      assert notify?(alert, @subscription)
+    end
 
-    bus_params = Map.merge(@base_params, %{"routes" => ["57A - 0"]})
-    assert %Subscription{informed_entities: [%InformedEntity{} | _]} = subscription(:bus, bus_params)
+    test "same route_type" do
+      alert = alert(%{"severity" => @alert_severity, "active_period" => @alert_active_period,
+                      "informed_entity" => [%{@alert_entity | "route_id" => nil, "direction_id" => nil,
+                                                              "stop_id" => nil}]})
+      assert notify?(alert, @subscription)
+    end
 
-    commuter_rail_params = Map.merge(@base_params, %{"destination" => "place-north", "direction_id" => "1",
-                                                    "origin" => "Melrose Highlands", "route_id" => "CR-Haverhill",
-                                                    "trips" => ["288"]})
-    assert %Subscription{informed_entities: [%InformedEntity{} | _]} = subscription(:commuter_rail, commuter_rail_params)
+    test "same route_id and route_type" do
+      alert = alert(%{"severity" => @alert_severity, "active_period" => @alert_active_period,
+                      "informed_entity" => [%{@alert_entity | "direction_id" => nil,
+                                                              "stop_id" => nil}]})
+      assert notify?(alert, @subscription)
+    end
 
-    ferry_params = Map.merge(@base_params, %{"destination" => "Boat-Long", "direction_id" => "1",
-                                            "origin" => "Boat-Charlestown", "route_id" => "Boat-F4",
-                                            "trips" => ["Boat-F4-Boat-Charlestown-08:00:00-weekday-1"]})
-    assert %Subscription{informed_entities: [%InformedEntity{} | _]} = subscription(:ferry, ferry_params)
+    test "same route_id, route_type and direction" do
+      alert = alert(%{"severity" => @alert_severity, "active_period" => @alert_active_period,
+                      "informed_entity" => [%{@alert_entity | "stop_id" => nil}]})
+      assert notify?(alert, @subscription)
+    end
+
+    # Vary Time / Days
+    test "similar: different time" do
+      alert = alert(%{"severity" => @alert_severity, "active_period" => @alert_later_period,
+                      "informed_entity" => [@alert_entity]})
+      refute notify?(alert, @subscription)
+    end
+
+    test "similar, different days (same time)" do
+      alert = alert(%{"severity" => @alert_severity, "active_period" => @alert_weekend_period,
+                      "informed_entity" => [@alert_entity]})
+      refute notify?(alert, @subscription)
+    end
+
+    # Vary Severity
+    test "similar: not severe enough" do
+      alert = alert(%{"severity" => severity_by_priority("low"), "active_period" => @alert_active_period,
+                      "informed_entity" => [@alert_entity]})
+      refute notify?(alert, @subscription)
+    end
+
+    test "similar: more severe" do
+      alert = alert(%{"severity" => severity_by_priority("high"), "active_period" => @alert_active_period,
+                      "informed_entity" => [@alert_entity]})
+      assert notify?(alert, @subscription)
+    end
+
+    # Vary Activities
+    test "similar: different activity" do
+      alert = alert(%{"severity" => @alert_severity, "active_period" => @alert_active_period,
+                      "informed_entity" => [%{@alert_entity | "activities" => ["NO_MATCH"]}]})
+      refute notify?(alert, @subscription)
+    end
+
+    # Vary Location
+    test "similar, different route_type" do
+      alert = alert(%{"severity" => @alert_severity, "active_period" => @alert_active_period,
+                      "informed_entity" => [%{@alert_entity | "route_type" => 2}]})
+      refute notify?(alert, @subscription)
+    end
+
+    test "similar, different route" do
+      alert = alert(%{"severity" => @alert_severity, "active_period" => @alert_active_period,
+                      "informed_entity" => [%{@alert_entity | "route_id" => "Blue"}]})
+      refute notify?(alert, @subscription)
+    end
+
+    test "similar, different direction" do
+      alert = alert(%{"severity" => @alert_severity, "active_period" => @alert_active_period,
+                      "informed_entity" => [%{@alert_entity | "direction_id" => 1}]})
+      refute notify?(alert, @subscription)
+    end
+
+    test "similar, different stop -- not in between" do
+      alert = alert(%{"severity" => @alert_severity, "active_period" => @alert_active_period,
+                      "informed_entity" => [%{@alert_entity | "stop_id" => "place-asmnl"}]})
+      refute notify?(alert, @subscription)
+    end
+
+    test "similar, different stop -- in between" do
+      alert = alert(%{"severity" => @alert_severity, "active_period" => @alert_active_period,
+                      "informed_entity" => [%{@alert_entity | "stop_id" => "place-cntsq"}]})
+      refute notify?(alert, @subscription)
+    end
+
+    # Roaming
+    test "similar, different direction (roaming)" do
+      alert = alert(%{"severity" => @alert_severity, "active_period" => @alert_active_period,
+                      "informed_entity" => [%{@alert_entity | "direction_id" => 1}]})
+      assert notify?(alert, @subscription_roaming)
+    end
+
+    test "similar, different stop -- in between (roaming)" do
+      alert = alert(%{"severity" => @alert_severity, "active_period" => @alert_active_period,
+                      "informed_entity" => [%{@alert_entity | "stop_id" => "place-cntsq"}]})
+      assert notify?(alert, @subscription_roaming)
+    end
   end
 
-  test "subway subscriptions" do
-    subway_params = Map.merge(@base_params, %{"destination" => "place-brdwy", "origin" => "place-harsq",
-                                             "roaming" => "false"})
-    subscription = subscription(:subway, subway_params)
-    activities = nil
-    trips = nil
-
-    assert length(SubscriptionFilterEngine.determine_recipients(alert(:all, subscription, activities, trips), [subscription], [])) == 1
-    assert length(SubscriptionFilterEngine.determine_recipients(alert(:not_route, subscription, activities, trips), [subscription], [])) == 0
-  end
+  defp notify?(alert, subscription), do: length(determine_recipients(alert, [subscription], [])) > 0
 end
--- apps/concierge_site/test/support/alert_factory.ex
+++ apps/concierge_site/test/support/alert_factory.ex
@@ -2,69 +2,48 @@ defmodule ConciergeSite.AlertFactory do
   @moduledoc """
   Simplify generation of alerts
   """
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+  import AlertProcessor.AlertParser, only: [parse_alert: 3]
+  alias AlertProcessor.ServiceInfoCache
 
-  alias AlertProcessor.{Model.InformedEntity, Model.Alert}
+  @one_day 86_400
 
-  @base_alert %Alert{header: "", id: "1", effect_name: "", last_push_notification: DateTime.utc_now(),
-                     informed_entities: [], active_period: []}
-  @base_informed_entity %InformedEntity{activities: InformedEntity.default_entity_activities()}
-
-  defp add_informed_entity(alert, subscription, activities, trips) do
-    informed_entitities = @base_informed_entity
-    |> add_route_type(subscription)
-    |> add_route(subscription)
-    |> add_activities(activities)
-    |> add_direction(subscription)
-    |> add_stops(subscription)
-    |> add_trips(trips)
-
-    %{alert | informed_entities: alert.informed_entities ++ informed_entitities}
+  def alert(alert_data) do
+    use_cassette "facilities_alerts", custom: true, clear_mock: true, match_requests_on: [:query] do
+      with {:ok, facilities_map} <- ServiceInfoCache.get_facility_map() do
+        parse_alert(ensure_alert_data_keys(alert_data), facilities_map, timestamp())
+      end
+    end
   end
 
-  defp add_stops(informed_entity, %{origin: origin, destination: destination}) do
-    [%{informed_entity | stop: origin}, %{informed_entity | stop: destination}]
-  end
-  defp add_stops(informed_entity, _), do: [informed_entity]
-
-  defp add_trips([informed_entity | _], nil), do: [informed_entity]
-  defp add_trips([informed_entity | _], trips), do: Enum.map(trips, & %{informed_entity | trip: &1})
-
-  defp time_to_datetime(time) do
-    {hour, minute, second} = Time.to_erl(time)
-    %{year: year, day: day, month: month} = Date.utc_today()
-    %DateTime{year: year, month: month, day: day, zone_abbr: "UTC", hour: hour, minute: minute, second: second,
-              microsecond: {0, 0}, utc_offset: 0, std_offset: 0, time_zone: "Etc/UTC"}
+  defp ensure_alert_data_keys(alert_data) do
+    %{"id" => "1",
+      "active_period" => [inclusive_active_period()],
+      "created_timestamp" => timestamp(),
+      "duration_certainty" => "KNOWN",
+      "effect_detail" => "SERVICE_CHANGE",
+      "header_text" => [%{"translation" => %{"text" => "Header Text", "language" => "en"}}],
+      "service_effect_text" => [%{"translation" => %{"text" => "Service Effect", "language" => "en"}}],
+      "severity" => inclusive_severity(),
+      "last_push_notification_timestamp" => timestamp()}
+    |> Map.merge(alert_data)
   end
 
-  defp add_direction(informed_entity, subscription), do: %{informed_entity | direction_id: subscription.direction_id}
+  defp inclusive_active_period(), do: %{"start" => timestamp(), "end" => timestamp() + (100 * 365 * 24 * 60 * 60)}
+  defp inclusive_severity(), do: 9
+  def timestamp(), do: 1514782800 # Monday, January 1, 2018 12:00:00 AM GMT-05:00 (EST)
 
-  defp add_route(informed_entity, subscription), do: %{informed_entity | route: subscription.route}
+  def alert_time(time), do: timestamp() + (time.hour * 60 * 60) + (time.minute * 60) + time.second
 
-  defp add_route_type(informed_entity, %{type: :subway}), do: %{informed_entity | route_type: 1}
-  defp add_route_type(informed_entity, %{type: :commuter_rail}), do: %{informed_entity | route_type: 2}
-  defp add_route_type(informed_entity, %{type: :bus}), do: %{informed_entity | route_type: 3}
-  defp add_route_type(informed_entity, %{type: :ferry}), do: %{informed_entity | route_type: 4}
-
-  defp add_activities(informed_entity, nil), do: informed_entity
-  defp add_activities(informed_entity, activities), do: %{informed_entity | activities: activities}
-
-  defp active_period(alert, subscription) do
-    %{alert | active_period: alert.active_period ++ [%{start: time_to_datetime(subscription.start_time),
-                                                       end: time_to_datetime(subscription.end_time)}]}
+  def active_period(start_time, end_time, :monday), do: %{"start" => alert_time(start_time), "end" => alert_time(end_time)}
+  def active_period(start_time, end_time, :sunday) do
+    %{"start" => alert_time(start_time) + (@one_day * 6), "end" => alert_time(end_time) + (@one_day * 6)}
+  end
+  def active_period(start_time, end_time, :saturday) do
+    %{"start" => alert_time(start_time) + (@one_day * 5), "end" => alert_time(end_time) + (@one_day * 5)}
   end
 
-  defp severity(alert, %{alert_priority_type: :high}), do: %{alert | severity: :minor}
-  defp severity(alert, %{alert_priority_type: :medium}), do: %{alert | severity: :moderate}
-  defp severity(alert, %{alert_priority_type: :low}), do: %{alert | severity: :severe}
-
-  def alert(:all, subscription, activities, trips) do
-    @base_alert
-    |> severity(subscription)
-    |> add_informed_entity(subscription, activities, trips)
-    |> active_period(subscription)
-  end
-
-  def alert(:not_route, subscription, activities, trips) do
-    alert(:all, %{subscription | route: "Not"}, activities, trips)
-  end
+  def severity_by_priority("high"), do: 7
+  def severity_by_priority("medium"), do: 5
+  def severity_by_priority("low"), do: 1
 end
