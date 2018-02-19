8806efa3b5d549281f491367fc0b84ae318a3994
zfletch <zfletch2@gmail.com>
2018-02-19 17:12:17 -0500

    use bus headsigns

--- apps/concierge_site/test/web/subscriptions/bus_routes_test.exs
+++ apps/concierge_site/test/web/subscriptions/bus_routes_test.exs
@@ -10,6 +10,24 @@ defmodule ConciergeSite.Subscriptions.BusRoutesTest do
           route_id: "57A",
           long_name: "57A Long Name",
           route_type: 3,
+          headsigns: %{
+            0 => ["Logan Airport", "Silver Line Way"],
+            1 => ["South Station"]
+          }
+        }
+      ]
+
+      select_options = BusRoutes.route_list_select_options(routes)
+
+      assert select_options == [{"Route 57A Long Name - South Station", "57A - 1"}, {"Route 57A Long Name - Logan Airport, Silver Line Way", "57A - 0"}]
+    end
+
+    test "uses inbound and outbound when there are no headsigns" do
+      routes = [
+        %AlertProcessor.Model.Route{
+          route_id: "57A",
+          long_name: "57A Long Name",
+          route_type: 3,
         }
       ]
 
