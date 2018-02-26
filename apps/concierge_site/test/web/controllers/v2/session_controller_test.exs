25318376aabcb23863ed9f59f873af5ca81d2f85
Ryan Mahoney <ryan@virtuecenter.com>
2018-02-26 14:21:03 -0500

    cover the case of redirecting when user already has trips saved

--- apps/concierge_site/test/web/controllers/v2/session_controller_test.exs
+++ apps/concierge_site/test/web/controllers/v2/session_controller_test.exs
@@ -1,7 +1,7 @@
 defmodule ConciergeSite.V2.SessionControllerTest do
   use ConciergeSite.ConnCase
   import AlertProcessor.Factory
-  alias AlertProcessor.{Model.User, Repo}
+  alias AlertProcessor.{Model.User, Model.Trip, Repo}
 
   @password "password1"
   @encrypted_password Comeonin.Bcrypt.hashpwsalt(@password)
@@ -16,6 +16,15 @@ defmodule ConciergeSite.V2.SessionControllerTest do
     params = %{"user" => %{"email" => user.email,"password" => @password}}
     conn = post(conn, v2_session_path(conn, :create), params)
     assert html_response(conn, 302) =~ "<a href=\"/v2/account/new\">"
+  end
+
+  test "POST /v2/login with trips", %{conn: conn} do
+    user = Repo.insert!(%User{email: "test@email.com", role: "user", encrypted_password: @encrypted_password})
+    Repo.insert!(%Trip{user_id: user.id, alert_priority_type: :low, relevant_days: [:monday], start_time: ~T[12:00:00],
+                       end_time: ~T[18:00:00], notification_time: ~T[11:00:00], station_features: [:accessibility]})
+    params = %{"user" => %{"email" => user.email,"password" => @password}}
+    conn = post(conn, v2_session_path(conn, :create), params)
+    assert html_response(conn, 302) =~ "<a href=\"/v2/trips\">"
   end
 
   test "POST /v2/login rejected", %{conn: conn} do
