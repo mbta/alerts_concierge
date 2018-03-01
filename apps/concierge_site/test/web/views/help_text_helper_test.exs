29c07f562790e63050bfd198417828fc5ec60c35
Ryan Mahoney <ryan@virtuecenter.com>
2018-03-01 16:44:41 -0500

    v2 help text

---
+++ apps/concierge_site/test/web/views/help_text_helper_test.exs
@@ -0,0 +1,15 @@
+defmodule ConciergeSite.HelpTextHelperTest do
+  @moduledoc false
+  use ExUnit.Case
+  alias ConciergeSite.HelpTextHelper
+
+  test "link/1" do
+    html = Phoenix.HTML.safe_to_string(HelpTextHelper.link("test"))
+    assert html == "<a class=\"helptext__link\" data-message-id=\"test\" data-type=\"help-link\" href=\"#show\"><i class=\"fa fa-question-circle helptext__link--icon\"></i></a>"
+  end
+
+  test "message/2" do
+    html = Phoenix.HTML.safe_to_string(HelpTextHelper.message("test", "This is a help message."))
+    assert html == "<div class=\"helptext__message\" data-message-id=\"test\" data-type=\"help-message\"><a class=\"helptext__message--close\" data-message-id=\"test\" data-type=\"close-help-text\" href=\"#close\"><i class=\"fa fa-times-circle helptext__message--close-icon\"></i></a>This is a help message.</div>"
+  end
+end
