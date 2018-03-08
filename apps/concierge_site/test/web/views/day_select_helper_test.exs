ca5821be9b05239cc9aaa04bc0a931754ebe2841
zfletch <zfletch2@gmail.com>
2018-03-08 11:56:01 -0500

    subscription preferences page
    Front-end of the subscription preferences page.

--- apps/concierge_site/test/web/views/day_select_helper_test.exs
+++ apps/concierge_site/test/web/views/day_select_helper_test.exs
@@ -9,7 +9,15 @@ defmodule ConciergeSite.DaySelectHelperTest do
     assert html =~ "<div class=\"day-selector\" data-selector=\"date\">"
     assert html =~ "<div class=\"day-header\">Mon</div>"
     assert html =~ "<input autocomplete=\"off\" name=\"foo[days][]\" type=\"checkbox\" value=\"monday\">"
+    assert html =~ "<input autocomplete=\"off\" name=\"foo[days][]\" type=\"checkbox\" value=\"tuesday\">"
     assert html =~ "<div class=\"group-part invisible-no-js\">"
     assert html =~ "<input autocomplete=\"off\" type=\"checkbox\" value=\"weekdays\">"
+  end
+
+  test "render/2" do
+    html = Phoenix.HTML.safe_to_string(DaySelectHelper.render(:foo, ["monday"]))
+
+    assert html =~ "<input autocomplete=\"off\" checked=\"checked\" name=\"foo[days][]\" type=\"checkbox\" value=\"monday\">"
+    assert html =~ "<input autocomplete=\"off\" name=\"foo[days][]\" type=\"checkbox\" value=\"tuesday\">"
   end
 end
