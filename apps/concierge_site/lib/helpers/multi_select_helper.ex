0b8366e8995d73819986b7864826534d11b09b3f
Ryan Mahoney <ryan@virtuecenter.com>
2018-04-24 16:55:16 -0400

    more dead code

--- apps/concierge_site/lib/helpers/multi_select_helper.ex
+++
@@ -1,9 +0,0 @@
-defmodule ConciergeSite.Helpers.MultiSelectHelper do
-  alias ConciergeSite.Subscriptions.Lines
-
-  def station_options(cr_stations, subway_stations) do
-    subway_stations
-    |> Kernel.++(cr_stations)
-    |> Lines.station_list_select_options()
-  end
-end
