035c04f77676f53c925e0599d8d8a7965875f850
Sebastian Abondano <sabondano1@gmail.com>
2018-04-26 21:02:40 -0400

    Edit green line branches display names
    Why:
    
    * We want green line branches to render as "Green Line B" and not "Green
    Line B Branch".
    * Asana link: https://app.asana.com/0/529741067494252/650983734352359
    
    This change addresses the need by:
    
    * Edits `RouteSelectHelper` and `TripCardHelper` to render green line
    branch names as described above.

--- apps/concierge_site/lib/views/route_select_helper.ex
+++ apps/concierge_site/lib/views/route_select_helper.ex
@@ -58,10 +58,10 @@ defmodule ConciergeSite.RouteSelectHelper do
   defp get_routes(:subway) do
     [{:red, "Red", "Red Line"},
      {:orange, "Orange", "Orange Line"},
-     {:"green-b", "Green-B", "Green Line B Branch"},
-     {:"green-c", "Green-C", "Green Line C Branch"},
-     {:"green-d", "Green-D", "Green Line D Branch"},
-     {:"green-e", "Green-E", "Green Line E Branch"},
+     {:"green-b", "Green-B", "Green Line B"},
+     {:"green-c", "Green-C", "Green Line C"},
+     {:"green-d", "Green-D", "Green Line D"},
+     {:"green-e", "Green-E", "Green Line E"},
      {:blue, "Blue", "Blue Line"},
      {:mattapan, "Mattapan", "Mattapan Trolley"}]
   end
