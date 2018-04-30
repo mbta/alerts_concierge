20496c2c5d34e68e563db117594111668037d30d
Ryan Mahoney <ryan@virtuecenter.com>
2018-04-30 17:04:41 -0400

    remove digest feature (#709)

--- apps/alert_processor/lib/helpers/date_time_helper.ex
+++ apps/alert_processor/lib/helpers/date_time_helper.ex
@@ -3,13 +3,9 @@ defmodule AlertProcessor.Helpers.DateTimeHelper do
   Module containing reusable datetime helpers for
   multi-step datetime functions.
   """
-  @seconds_in_a_day 86_400
   @time_zone "America/New_York"
 
-  alias Calendar.Date, as: D
   alias Calendar.DateTime, as: DT
-  alias Calendar.Time, as: T
-  alias Calendar.Date, as: D
   alias Calendar.Strftime
 
   @spec time_without_zone(String.t) :: Time.t
@@ -69,102 +65,6 @@ defmodule AlertProcessor.Helpers.DateTimeHelper do
          {:ok, output} <- Strftime.strftime(local_datetime, "%l:%M%P") do
       output
     end
-  end
-
-  @doc """
-  Takes 4 arguments:
-  1. The interval length in seconds (1 week)
-  2. The day of the week the digest goes out
-  3. The time of day the digest goes out
-  4. {current_date, current_time}
-
-  It then calculates the number of seconds to the next Date/Time the digest would go out.
-  Accounts for negative days (current day of week is > day of week digest goes out)
-  Accounts for negative time (current time is > time of day digest goes out)
-  """
-  @spec seconds_until_next_digest(integer, integer, Time.t, {Date.t, Time.t}) :: integer
-  def seconds_until_next_digest(interval, day_of_week, time_of_day, {date, time}) do
-    difference = difference_in_days_as_seconds(day_of_week, date) +
-                 difference_in_time_as_seconds(time_of_day, time)
-    if difference >= 0 do
-      difference
-    else
-      interval + difference
-    end
-  end
-
-  defp difference_in_days_as_seconds(day_of_week, today) do
-    current_day_of_week = today |> Date.day_of_week
-    (day_of_week - current_day_of_week) * @seconds_in_a_day
-  end
-
-  defp difference_in_time_as_seconds(time_of_day, time_now) do
-    T.diff(time_of_day, time_now)
-  end
-
-  @doc """
-  Returns a tuple of the datetime of the upcoming weekend.
-  If called on a weekend, will go back to start of current weekend.
-  """
-  @spec upcoming_weekend(DateTime.t | nil, String.t) :: {DateTime.t, DateTime.t}
-  def upcoming_weekend(time \\ nil, time_zone \\ @time_zone) do
-    today = time || DT.now!(time_zone)
-
-    start_time = today
-    |> Map.merge(%{
-      hour: 2,
-      minute: 30,
-      second: 1,
-      microsecond: {0, 0}
-    })
-    |> DT.add!(seconds_till_saturday(today))
-    end_time = DT.add!(start_time, @seconds_in_a_day * 2 - 1)
-
-    {start_time, end_time}
-  end
-
-  @spec seconds_till_saturday(DateTime.t) :: number
-  defp seconds_till_saturday(date_time) do
-    day_of_week = date_time
-    |> DT.to_date
-    |> D.day_of_week
-    if day_of_week >= 6 do
-      (6 - day_of_week) * @seconds_in_a_day + 7 * @seconds_in_a_day
-    else
-      (6 - day_of_week) * @seconds_in_a_day
-    end
-  end
-
-  @doc """
-  Returns a tuple of DateTimes of the upcoming week
-  """
-  @spec upcoming_week(DateTime.t | nil) :: {DateTime.t, DateTime.t}
-  def upcoming_week(time \\ nil) do
-    {_start, end_of_weekend} = upcoming_weekend(time)
-    start_time = DT.add!(end_of_weekend, 1)
-    end_time = DT.add!(start_time, @seconds_in_a_day * 5 - 1)
-
-    {start_time, end_time}
-  end
-
-  @doc """
-  Returns a tuple of DateTimes of the next weekend (after upcoming week)
-  """
-  @spec next_weekend(DateTime.t | nil) :: {DateTime.t, DateTime.t}
-  def next_weekend(time \\ nil) do
-    {_start, end_of_week} = upcoming_week(time)
-    start_time = DT.add!(end_of_week, 1)
-    end_time = DT.add!(start_time, @seconds_in_a_day * 2 - 1)
-
-    {start_time, end_time}
-  end
-
-  @spec future(DateTime.t | nil, String.t) :: {DateTime.t, DateTime.t}
-  def future(time \\ nil, time_zone \\ @time_zone) do
-    {_start, end_of_next_weekend} = next_weekend(time)
-    start_time = DT.add!(end_of_next_weekend, 1)
-    end_time = DT.from_erl!({{3000, 01, 01}, {0, 0, 0}}, time_zone)
-    {start_time, end_time}
   end
 
   @doc "Returns full name of month for a DateTime"
--- apps/concierge_site/test/lib/dissemination/mailer_interface_test.exs
+++ apps/concierge_site/test/lib/dissemination/mailer_interface_test.exs
@@ -1,7 +1,7 @@
 defmodule ConciergeSite.Dissemination.MailerInterfaceTest do
   use ConciergeSite.DataCase
   alias ConciergeSite.Dissemination.MailerInterface
-  alias AlertProcessor.Model.{Alert, Digest, DigestDateGroup, DigestMessage, InformedEntity, Notification, User}
+  alias AlertProcessor.Model.{Alert, InformedEntity, Notification}
 
   describe "send_notification_email" do
     test "receives a notification and sends an email" do
@@ -25,49 +25,6 @@ defmodule ConciergeSite.Dissemination.MailerInterfaceTest do
       assert sent_email.text_body =~ "Red line delay"
       assert sent_email.text_body =~ "Red line inbound from Alewife station closure"
       assert sent_email.text_body =~ "There is a fire at south station so it is closed"
-    end
-  end
-
-  describe "send_digest_email" do
-    test "receives a digest message and sends an email" do
-      now = Calendar.DateTime.now!("America/New_York")
-      saturday = Calendar.DateTime.from_erl!({{2017, 05, 26}, {0, 0, 0}}, "America/New_York")
-      end_sunday = Calendar.DateTime.from_erl!({{2017, 05, 28}, {02, 30, 00}}, "America/New_York")
-
-      digest_date_group = %DigestDateGroup{
-        upcoming_weekend: %{
-          timeframe: {saturday, end_sunday},
-          alert_ids: ["1"]
-        },
-        upcoming_week: %{
-          timeframe: {now, now},
-          alert_ids: ["1"]
-        },
-        next_weekend: %{
-          timeframe: {now, now},
-          alert_ids: ["1"]
-        },
-        future: %{
-          timeframe: {now, now},
-          alert_ids: ["1"]
-        }
-      }
-      alert = %Alert{
-        id: "1",
-        header: "This is a Test",
-        service_effect: "Service Effect",
-        informed_entities: [%InformedEntity{route_type: 1, route: "Red"}]
-      }
-      user = %User{email: "abc@123.com"}
-      digest = %Digest{user: user, alerts: [alert], digest_date_group: digest_date_group}
-      message = DigestMessage.from_digest(digest)
-
-      {:reply, sent_email, nil} = MailerInterface.handle_call({:send_digest_email, message}, nil, nil)
-      assert sent_email.to == [{nil, user.email}]
-      assert sent_email.text_body =~ "This is a Test"
-      assert sent_email.text_body =~ "Service Effect"
-      assert sent_email.text_body =~ "https://www.mbta.com/"
-      assert sent_email.text_body =~ "This Weekend, May 26 - 27"
     end
   end
 end
