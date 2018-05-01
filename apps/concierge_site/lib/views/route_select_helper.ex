ebfa287b87425cc68c2e92558c28038ec19ca7c1
Ryan Mahoney <ryan@virtuecenter.com>
2018-05-01 10:14:30 -0400

    select Green virtual route from route selector

--- apps/concierge_site/lib/views/route_select_helper.ex
+++ apps/concierge_site/lib/views/route_select_helper.ex
@@ -58,10 +58,7 @@ defmodule ConciergeSite.RouteSelectHelper do
   defp get_routes(:subway) do
     [{:red, "Red", "Red Line"},
      {:orange, "Orange", "Orange Line"},
-     {:"green-b", "Green-B", "Green Line B"},
-     {:"green-c", "Green-C", "Green Line C"},
-     {:"green-d", "Green-D", "Green Line D"},
-     {:"green-e", "Green-E", "Green Line E"},
+     {:green, "Green", "Green Line"},
      {:blue, "Blue", "Blue Line"},
      {:mattapan, "Mattapan", "Mattapan Trolley"}]
   end
