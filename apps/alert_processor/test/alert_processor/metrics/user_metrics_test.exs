f54066fe8d16e454bf496382adb93eb6c9701511
Ryan Mahoney <ryan@virtuecenter.com>
2018-05-09 14:40:45 -0400

    log count of users with and without phone numbers every 5 mins

---
+++ apps/alert_processor/test/alert_processor/metrics/user_metrics_test.exs
@@ -0,0 +1,19 @@
+defmodule AlertProcessor.Metrics.UserMetricsTest do
+  @moduledoc false
+  use AlertProcessor.DataCase
+  import AlertProcessor.Factory
+  alias AlertProcessor.Metrics.UserMetrics
+
+  test "counts_by_type/0" do
+    # insert two default users with phone numbers
+    insert(:user)
+    insert(:user)
+    
+    # insert one user without a phone number
+    insert(:user, %{phone_number: nil})
+    
+    [phone_count, email_count] = UserMetrics.counts_by_type()
+    assert phone_count == 2
+    assert email_count == 1
+  end
+end
