9a88b61dcab1ca36730bac3e503681553a67e637
Ryan Mahoney <ryan@virtuecenter.com>
2018-05-14 14:31:38 -0400

    send sms confirmation when user updates options on account

--- apps/concierge_site/lib/users/confirmation_message.ex
+++ apps/concierge_site/lib/users/confirmation_message.ex
@@ -5,23 +5,14 @@ defmodule ConciergeSite.ConfirmationMessage do
   alias AlertProcessor.{Aws.AwsClient, Model.Notification, NotificationSmser}
   alias ConciergeSite.Dissemination.{Email, Mailer}
 
-  @doc """
-  Sends email or SMS to uesr based on if they have a phone number
-  """
-  def send_confirmation(user) do
-    unless is_nil(user.phone_number) do
-      send_sms_confirmation(user.phone_number)
-    end
-    send_email_confirmation(user)
-  end
-
-  defp send_email_confirmation(user) do
+  def send_email_confirmation(user) do
     user
     |> Email.confirmation_email()
     |> Mailer.deliver_later
   end
 
-  defp send_sms_confirmation(phone_number) do
+  def send_sms_confirmation("", _), do: {:ok, nil}
+  def send_sms_confirmation(phone_number, "true") do
     %Notification{
       header: "You have been subscribed to MBTA alerts. To stop receiving texts, reply STOP (cannot resubscribe for 30 days) or visit alerts.mbta.com. Data rates may apply.",
       phone_number: phone_number
@@ -29,4 +20,5 @@ defmodule ConciergeSite.ConfirmationMessage do
     |> NotificationSmser.notification_sms()
     |> AwsClient.request()
   end
+  def send_sms_confirmation(_, _), do: {:ok, nil}
 end
