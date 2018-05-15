db755c7666f5dc7bc47bd12c68d188416ddcc7d0
Ryan Mahoney <ryan@virtuecenter.com>
2018-05-15 14:24:43 -0400

    shift end-time automatically (#751)

--- apps/concierge_site/assets/js/custom-time-select.js
+++ apps/concierge_site/assets/js/custom-time-select.js
@@ -1,4 +1,37 @@
 import flatpickr from "flatpickr";
+import { timeToInt, timeFromInt } from "time-number";
+
+const oneHourInSeconds = 3600;
+const oneDayInSeconds = 86400;
+
+// when the user is changing the start time, shift the end time 1 hour forward when the times are the same
+const shiftEndTime = e => {
+  const startTimeEl = e.changedEl;
+  const startTimeElId = startTimeEl.getAttribute("id");
+  const isStartTime = startTimeElId.indexOf("_start_time") != -1 ? true : false;
+  if (!isStartTime) {
+    return;
+  }
+
+  const endTimeElId = startTimeElId.replace("_start_time", "_end_time");
+  const endTimeEl = document.getElementById(endTimeElId);
+  const startTime = startTimeEl.value.trim();
+  const endTime = endTimeEl.value.trim();
+
+  if (startTime != endTime) {
+    return;
+  }
+
+  const startTimeInSeconds = timeToInt(startTime);
+  const newEndTimeInSeconds =
+    startTimeInSeconds + oneHourInSeconds > oneDayInSeconds - 1
+      ? 0
+      : startTimeInSeconds + oneHourInSeconds;
+  endTimeEl.value = timeFromInt(newEndTimeInSeconds, {
+    format: 12,
+    leadingZero: false
+  });
+};
 
 export default pubsub => {
   const config = {
@@ -14,15 +47,16 @@ export default pubsub => {
           ? "return"
           : "start";
 
-      document
-        .getElementById(instance.element.id)
-        .setAttribute("value", dateStr);
+      const changedEl = document.getElementById(instance.element.id);
 
       pubsub.publishSync("time-change", {
-        mode: mode
+        mode: mode,
+        changedEl: changedEl
       });
     }
   };
+
+  pubsub.subscribe("time-change", shiftEndTime);
 
   [...document.querySelectorAll("input[data-type='time']")].forEach(input => {
     flatpickr(input, config);
--- apps/concierge_site/test/web/views/time_helper_test.exs
+++ apps/concierge_site/test/web/views/time_helper_test.exs
@@ -42,4 +42,18 @@ defmodule ConciergeSite.TimeHelperTest do
 
     assert TimeHelper.travel_time_options() == expected
   end
+  
+  describe "format_time/1" do
+    test "converts time into readable format" do
+      actual = TimeHelper.format_time(~T[01:00:00.000000])
+      expected = " 1:00 AM"
+      assert actual == expected
+    end
+
+    test "converts nil to empty string" do
+      actual = TimeHelper.format_time(nil)
+      expected = ""
+      assert actual == expected
+    end
+  end
 end
