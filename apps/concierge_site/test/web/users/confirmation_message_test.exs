d51ae5b596178429b54576b3bd38c14a994d71f2
Ryan Mahoney <ryan@virtuecenter.com>
2018-05-15 13:10:55 -0400

    send sms confirmation when user updates options on account (#750)

--- apps/concierge_site/test/web/users/confirmation_message_test.exs
+++ apps/concierge_site/test/web/users/confirmation_message_test.exs
@@ -4,25 +4,43 @@ defmodule ConciergeSite.ConfirmationMessageTest do
   alias ConciergeSite.Dissemination.Email
   alias ConciergeSite.ConfirmationMessage
 
-  describe "send_confirmation/1" do
-    test "sends email to user without phone number" do
+  describe "send_email_confirmation/1" do
+    test "sends email to user" do
       user = insert(:user, email: "test@test.com", phone_number: nil)
 
-      ConfirmationMessage.send_confirmation(user)
+      ConfirmationMessage.send_email_confirmation(user)
 
       email = Email.confirmation_email(user)
       refute_received :publish
       assert_delivered_with(to: [{nil, "test@test.com"}])
       assert email.html_body =~ "Thanks for signing up for T-Alerts Beta!"
     end
+  end
 
-    test "sends SMS and email to user with phone number" do
+  describe "send_sms_confirmation/1" do
+    test "sends SMS to user with phone number" do
       user = insert(:user, email: "test@test.com", phone_number: "5556667777")
 
-      ConfirmationMessage.send_confirmation(user)
+      expected = %{body: %{message_id: "123", request_id: "345"}}
+      {:ok, actual} = ConfirmationMessage.send_sms_confirmation(user.phone_number, "true")
 
-      assert_received :publish
-      assert_delivered_with(to: [{nil, "test@test.com"}])
+      assert actual == expected
+    end
+
+    test "does not send SMS to user with phone number but no opt-in" do
+      user = insert(:user, email: "test@test.com", phone_number: "5556667777")
+
+      {:ok, actual} = ConfirmationMessage.send_sms_confirmation(user.phone_number, "false")
+
+      assert actual == nil
+    end
+
+    test "does not send SMS to user with empty phone number" do
+      user = insert(:user, email: "test@test.com", phone_number: "")
+
+      {:ok, actual} = ConfirmationMessage.send_sms_confirmation(user.phone_number, "true")
+
+      assert actual == nil
     end
   end
 end
