5998b51a652d91e48867c31cca10dcdcd8ae204c
Sebastian Abondano <sabondano1@gmail.com>
2018-06-18 16:57:44 -0400

    Unnecessary call in NotificationWindowFilter
    Why:
    
    * To improve matching time. Not a huge deal, but this also makes it
    easier to "freeze" time when load testing.
    * Asana link: https://app.asana.com/0/529741067494252/714677169100846
    
    This change addresses the need by:
    
    * Editing `SubscriptionFilterEngine.determine_recipients/4` to pass the
    current datetime to the `@notification_window_filter`
    (`NotificationWindowFilter`)
    * Editing `NotificationWindowFilter.filter/2` to not call
    `Calendar.DateTime.now!("America/New_York")` and require a current
    datetime as the second argument. Before, the second argument was
    optional.
    * Editing `NotificationWindowFilterMock` to account for the changes
    above.

--- apps/alert_processor/test/support/mocks/notification_window_filter_mock.ex
+++ apps/alert_processor/test/support/mocks/notification_window_filter_mock.ex
@@ -1,5 +1,5 @@
 defmodule AlertProcessor.NotificationWindowFilterMock do
   @moduledoc false
 
-  def filter(subscriptions), do: subscriptions
+  def filter(subscriptions, _now), do: subscriptions
 end
