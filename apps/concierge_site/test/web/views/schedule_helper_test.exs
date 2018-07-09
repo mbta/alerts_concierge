b2c756f791df716f03a78daa4acf1ccdebbd1ac6
Ryan Mahoney <ryan@virtuecenter.com>
2018-07-09 12:59:07 -0400

    fix another test

--- apps/concierge_site/test/web/views/schedule_helper_test.exs
+++ apps/concierge_site/test/web/views/schedule_helper_test.exs
@@ -32,6 +32,6 @@ defmodule ConciergeSite.ScheduleHelperTest do
     assert html =~ "Which Commuter Rail trains would you like alerts about?"
     assert html =~ "Which Ferry boats would you like alerts about?"
     assert html =~ "Train 537 from Auburndale, 11:58pm"
-    assert html =~ "Boston (Long Wharf) from  6:30am"
+    assert html =~ "Ferry from Boston (Long Wharf),  6:30am"
   end
 end
