7f596369e858d8a491d1d3b2b20afeba281154a8
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2018-07-16 11:02:34 -0400

    Update AlertParserMock to handle all duration types
    This fixes errors that are occasionally thrown when running test suite.

--- apps/alert_processor/test/support/mocks/alert_parser_mock.ex
+++ apps/alert_processor/test/support/mocks/alert_parser_mock.ex
@@ -12,11 +12,23 @@ defmodule AlertProcessor.AlertParserMock do
   process_alerts/1 send messages to self for tests to be able
   to verify that process_alerts has properly been called.
   """
-  def process_alerts(:recent) do
-    send self(), :processed_alerts
-    [{:ok, []}]
+  def process_alerts(:older) do
+    send_to_self()
   end
+
+  def process_alerts(:recent) do
+    send_to_self()
+  end
+
+  def process_alerts(:anytime) do
+    send_to_self()
+  end
+
   def process_alerts() do
+    send_to_self()
+  end
+
+  defp send_to_self do
     send self(), :processed_alerts
     [{:ok, []}]
   end
