d6ec0bf0ed229fedd22f71fffa81a63c7cd7e7ed
Ryan Mahoney <ryan@virtuecenter.com>
2018-09-24 15:06:53 -0400

    update style of 404 and 500

--- apps/concierge_site/lib/views/error_view.ex
+++ apps/concierge_site/lib/views/error_view.ex
@@ -1,10 +1,6 @@
 defmodule ConciergeSite.ErrorView do
   use ConciergeSite.Web, :view
 
-  def render("500.html", _assigns) do
-    "Internal server error"
-  end
-
   # In case no render clause matches or no
   # template is found, let's render it as 500
   def template_not_found(_template, assigns) do
--- apps/concierge_site/test/web/views/error_view_test.exs
+++ apps/concierge_site/test/web/views/error_view_test.exs
@@ -11,7 +11,7 @@ defmodule ConciergeSite.ErrorViewTest do
 
   test "renders 404.html", %{conn: conn} do
     assert render_to_string(ConciergeSite.ErrorView, "404.html", conn: conn) =~
-             "Your stop cannot be found. This page is no longer in service."
+             "Oops! This stop can not be found."
   end
 
   test "renders 403.html", %{conn: conn} do
@@ -21,7 +21,7 @@ defmodule ConciergeSite.ErrorViewTest do
 
   test "render 500.html", %{conn: conn} do
     assert render_to_string(ConciergeSite.ErrorView, "500.html", conn: conn) ==
-             "Internal server error"
+             "Sorry, we’re currently out of service"
   end
 
   test "render any other", %{conn: conn} do
