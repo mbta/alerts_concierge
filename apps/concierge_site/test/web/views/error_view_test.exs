976bb5ab3091aac0e9e0659d26de95789343c0c7
Ryan Mahoney <ryan@virtuecenter.com>
2018-09-24 15:33:44 -0400

    fix tests

--- apps/concierge_site/test/web/views/error_view_test.exs
+++ apps/concierge_site/test/web/views/error_view_test.exs
@@ -20,12 +20,12 @@ defmodule ConciergeSite.ErrorViewTest do
   end
 
   test "render 500.html", %{conn: conn} do
-    assert render_to_string(ConciergeSite.ErrorView, "500.html", conn: conn) ==
+    assert render_to_string(ConciergeSite.ErrorView, "500.html", conn: conn) =~
              "Sorry, we’re currently out of service"
   end
 
   test "render any other", %{conn: conn} do
-    assert render_to_string(ConciergeSite.ErrorView, "505.html", conn: conn) ==
-             "Internal server error"
+    assert render_to_string(ConciergeSite.ErrorView, "505.html", conn: conn) =~
+             "Sorry, we’re currently out of service"
   end
 end
