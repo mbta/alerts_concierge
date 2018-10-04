938861d8b08e99d8355ddeeab95de58a7704cd3f
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2018-10-04 11:12:15 -0400

    Remove unused functions from alert

--- apps/alert_processor/lib/model/alert.ex
+++ apps/alert_processor/lib/model/alert.ex
@@ -58,87 +58,6 @@ defmodule AlertProcessor.Model.Alert do
           reminder_times: [DateTime.t()] | nil
         }
 
-  @route_types %{
-    0 => "Subway",
-    1 => "Subway",
-    2 => "Commuter Rail",
-    3 => "Bus",
-    4 => "Ferry"
-  }
-
-  @severity_map %{
-    "Subway" => %{
-      "Extra Service" => 2,
-      "Schedule Change" => 3,
-      "Shuttle" => 3,
-      "Station Closure" => 3,
-      "Suspension" => 3
-    },
-    "Commuter Rail" => %{
-      "Cancellation" => 3,
-      "Delay" => 2,
-      "Extra Service" => 1,
-      "Schedule Change" => 3,
-      "Shuttle" => 3,
-      "Station Closure" => 3,
-      "Suspension" => 3,
-      "Track Change" => 1
-    },
-    "Bus" => %{
-      "Detour" => 3,
-      "Extra Service" => 1,
-      "Schedule Change" => 3,
-      "Snow Route" => 3,
-      "Stop Closure" => 2,
-      "Stop Move" => 1,
-      "Suspension" => 3
-    },
-    "Ferry" => %{
-      "Cancellation" => 3,
-      "Delay" => 2,
-      "Dock Closure" => 3,
-      "Extra Service" => 2,
-      "Schedule Change" => 2,
-      "Shuttle" => 3,
-      "Suspension" => 3
-    },
-    "Systemwide" => %{
-      "Delay" => 3,
-      "Extra Service" => 2,
-      "Policy Change" => 3,
-      "Service Change" => 3,
-      "Suspension" => 3,
-      "Amber Alert" => 3
-    },
-    "Facility" => %{
-      "Access Issue" => 3
-    }
-  }
-
-  @doc """
-  return the numeric value for severity for a given alert. the higher the number
-  the more severe.
-  """
-  @spec severity_value(__MODULE__.t()) :: integer | nil
-  def severity_value(%__MODULE__{
-        severity: severity,
-        effect_name: effect_name,
-        informed_entities: informed_entities
-      }) do
-    informed_entities
-    |> Enum.map(fn informed_entity ->
-      mode =
-        case informed_entity do
-          %{route_type: rt, route: nil} when not is_nil(rt) -> "Systemwide"
-          %{route_type: rt, route: r} when not is_nil(rt) and not is_nil(r) -> route_string(rt)
-          %{stop: s} when not is_nil(s) -> "Facility"
-        end
-
-      priority_value(mode, effect_name, severity)
-    end)
-    |> Enum.max()
-  end
-
   @doc """
   parse alert active periods and return as timeframe map for comparison
   with subscriptions
@@ -202,29 +121,6 @@ defmodule AlertProcessor.Model.Alert do
   end
 
   defp timeframe_map(%{start: _}), do: true
-
-  @spec route_string(integer) :: String.t()
-  defp route_string(route_type) do
-    @route_types[route_type]
-  end
-
-  @spec priority_value(String.t(), String.t(), atom) :: integer
-  defp priority_value(route_type, effect_name, severity) do
-    @severity_map[route_type][effect_name] || default_severity_value(severity)
-  end
-
-  defp default_severity_value(:minor), do: 1
-  defp default_severity_value(:moderate), do: 2
-  defp default_severity_value(:severe), do: 3
-  defp default_severity_value(:extreme), do: 4
-
-  def facility_alert?(%__MODULE__{informed_entities: ies}) do
-    Enum.any?(ies, &(InformedEntity.entity_type(&1) == :amenity))
-  end
-
-  def advance_notice_in_seconds(%__MODULE__{duration_certainty: {:estimated, estimated_duration}}) do
-    Enum.min([3600, estimated_duration])
-  end
 
   def commuter_rail_alert?(%__MODULE__{informed_entities: ies}) when is_list(ies) do
     Enum.any?(ies, &(&1.route_type == 2))
--- apps/alert_processor/test/alert_processor/model/alert_test.exs
+++ apps/alert_processor/test/alert_processor/model/alert_test.exs
@@ -1,67 +1,7 @@
 defmodule AlertProcessor.Model.AlertTest do
+  @moduledoc false
   use ExUnit.Case, async: true
   alias AlertProcessor.Model.{Alert, InformedEntity}
-
-  @ie1 %InformedEntity{route_type: 0, route: nil}
-  @ie2 %InformedEntity{stop: "place-nqnce", facility_type: :elevator}
-  @ie3 %InformedEntity{route_type: 0, route: "Red"}
-  @ie4 %InformedEntity{stop: "place-nqncy"}
-
-  describe "severity_value/1" do
-    test "Systemwide" do
-      alert = %Alert{severity: :minor, informed_entities: [@ie1]}
-      assert Alert.severity_value(alert) == 1
-    end
-
-    test "Facility" do
-      alert = %Alert{severity: :minor, informed_entities: [@ie4]}
-      assert Alert.severity_value(alert) == 1
-    end
-
-    test "Other route" do
-      alert = %Alert{severity: :minor, informed_entities: [@ie3]}
-      assert Alert.severity_value(alert) == 1
-    end
-
-    test "Effect name overrides default severity" do
-      alert = %Alert{severity: :minor, effect_name: "Shuttle", informed_entities: [@ie3]}
-      assert Alert.severity_value(alert) == 3
-    end
-
-    test "Extreme severity" do
-      alert = %Alert{severity: :extreme, informed_entities: [@ie3]}
-      assert Alert.severity_value(alert) == 4
-    end
-  end
-
-  describe "facility_alert?/1" do
-    test "it returns true if any informed_entity has a facility_type" do
-      alert = %Alert{informed_entities: [@ie1, @ie2, @ie3]}
-      assert true == Alert.facility_alert?(alert)
-    end
-
-    test "it returns false if no informed_entity has a facility_type" do
-      alert = %Alert{informed_entities: [@ie1, @ie3]}
-      assert false == Alert.facility_alert?(alert)
-    end
-
-    test "it returns false if no informed_entities" do
-      alert = %Alert{informed_entities: []}
-      assert false == Alert.facility_alert?(alert)
-    end
-  end
-
-  describe "advance_notice_in_seconds/1" do
-    test "returns the default value if less than estimated duration" do
-      alert = %Alert{duration_certainty: {:estimated, 7200}}
-      assert 3600 = Alert.advance_notice_in_seconds(alert)
-    end
-
-    test "returns the estimated duration if lower than the default minor value" do
-      alert = %Alert{duration_certainty: {:estimated, 1800}}
-      assert 1800 = Alert.advance_notice_in_seconds(alert)
-    end
-  end
 
   describe "commuter_rail_alert?/1" do
     test "with commuter_rail informed_entities" do
