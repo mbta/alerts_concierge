073549ddfa8bc6ef970b3756b0b7b7f406f2febe
Cora Grant <394835+x@users.noreply.github.com>
2020-03-27 11:15:54 -0400

    Remove unused worker pool configuration
    * The configs `rate_limit` and `rate_limit_scale` are not referenced
      anywhere in the current code, and their corresponding env vars do not
      appear in any deployed environment.
    
    * Poolboy's `max_overflow` option only comes into effect when a process
      attempts to check out a worker from the pool but there are none free.
      We never check out workers from this pool, as they work independently
      and constantly in the background; we are essentially using Poolboy as
      a slightly fancier `DynamicSupervisor`.
    
    * `pool_size` is renamed to `notification_workers` to make the scope of
      the configuration more clear.

--- apps/alert_processor/config/test.exs
+++ apps/alert_processor/config/test.exs
@@ -27,9 +27,7 @@ config :alert_processor, database_url: {:system, "DATABASE_URL_TEST"}
 
 config :alert_processor, :notification_window_filter, AlertProcessor.NotificationWindowFilterMock
 
-config :alert_processor,
-  pool_size: 0,
-  overflow: 0
+config :alert_processor, notification_workers: 0
 
 config :exvcr,
   vcr_cassette_library_dir: "test/fixture/vcr_cassettes",
