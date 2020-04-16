52e59455e9d9f9589a99a2eec23f62c4aebf62da
Cora Grant <394835+x@users.noreply.github.com>
2020-04-16 14:32:27 -0400

    Enable Hackney request tracing
    This is to determine whether the apparent slow requests we see with SES
    are actually a result of the server taking a long time to respond after
    the request is made, or something else.

--- apps/alert_processor/lib/alert_processor.ex
+++ apps/alert_processor/lib/alert_processor.ex
@@ -6,6 +6,7 @@ defmodule AlertProcessor do
   def start(_type, _args) do
     link = AlertProcessor.Supervisor.start_link()
     Application.get_env(:alert_processor, :migration_task).migrate()
+    :hackney_trace.enable(:max, :io)
     link
   end
 end
--- apps/alert_processor/test/test_helper.exs
+++ apps/alert_processor/test/test_helper.exs
@@ -1,4 +1,5 @@
 {:ok, _} = Application.ensure_all_started(:ex_machina)
+:hackney_trace.disable()
 
 ExUnit.configure(exclude: [pending: true])
 ExUnit.start()
