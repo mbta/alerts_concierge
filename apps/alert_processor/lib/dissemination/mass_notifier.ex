3feac1f462731b79c77a83fe62224ebd1663f7c0
Cora Grant <394835+x@users.noreply.github.com>
2020-04-23 15:54:32 -0400

    Save and enqueue notifications in batches
    Local testing indicates that simply wrapping the existing notification
    saving code in transactions, with a size roughly equal to the number of
    sending workers, should provide a significant speed boost.

--- apps/alert_processor/lib/dissemination/mass_notifier.ex
+++ apps/alert_processor/lib/dissemination/mass_notifier.ex
@@ -3,8 +3,14 @@ defmodule AlertProcessor.Dissemination.MassNotifier do
 
   alias AlertProcessor.SendingQueue
   alias AlertProcessor.Model.Notification
+  alias AlertProcessor.Repo
 
   require Logger
+
+  # Save and enqueue notifications in batches equal to (at least) the number of sending workers,
+  # so all workers will have something to do while we save the next batch. The test environment
+  # sets the number of workers to 0, so we need to ensure the batch size is at least 1.
+  @batch_size max(1, Application.fetch_env!(:alert_processor, :notification_workers))
 
   @doc """
   Saves and enqueues a list of notifications. Returns the saved notifications. Any that fail to
@@ -19,29 +25,47 @@ defmodule AlertProcessor.Dissemination.MassNotifier do
   def save_and_enqueue(notifications) do
     log("event=start")
 
-    notifications =
-      Enum.map(notifications, fn notification ->
-        save_start = now()
-
-        case Notification.save(notification, :sent) do
-          {:ok, notification} ->
-            log("event=save notification=#{notification.id} time=#{now() - save_start}")
-
-            enqueue_start = now()
-            SendingQueue.enqueue(notification)
-            log("event=enqueue notification=#{notification.id} time=#{now() - enqueue_start}")
-
-            notification
-
-          {:error, changeset} ->
-            Logger.warn("notification insert failed: #{inspect(changeset)}")
-            nil
-        end
-      end)
-      |> Enum.reject(&is_nil/1)
+    saved_notifications =
+      notifications
+      |> Stream.chunk_every(@batch_size)
+      |> Stream.map(&save_batch/1)
+      |> Enum.flat_map(&enqueue_batch/1)
 
     log("event=finish")
 
+    saved_notifications
+  end
+
+  defp save_batch(notifications) do
+    batch_start = now()
+
+    {:ok, saved_notifications} =
+      Repo.transaction(fn ->
+        notifications
+        |> Stream.map(fn notification ->
+          save_start = now()
+
+          case Notification.save(notification, :sent) do
+            {:ok, notification} ->
+              log("event=save notification=#{notification.id} time=#{now() - save_start}")
+              notification
+
+            {:error, changeset} ->
+              Logger.warn("notification insert failed: #{inspect(changeset)}")
+              nil
+          end
+        end)
+        |> Enum.reject(&is_nil/1)
+      end)
+
+    log("event=commit time=#{now() - batch_start}")
+    saved_notifications
+  end
+
+  defp enqueue_batch(notifications) do
+    enqueue_start = now()
+    :ok = SendingQueue.list_enqueue(notifications)
+    log("event=enqueue time=#{now() - enqueue_start}")
     notifications
   end
 
