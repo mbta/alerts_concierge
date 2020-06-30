a3504471b7741863b43e1fbcca4e70e7e19e94a6
Ryan Mahoney <ryan.mahoney@outlook.com>
2020-06-30 09:55:22 -0400

    decrease worker count to check if improves errors

--- apps/alert_processor/config/config.exs
+++ apps/alert_processor/config/config.exs
@@ -54,7 +54,7 @@ config :alert_processor,
      "postgresql://postgres:postgres@localhost:5432/alert_concierge_dev"}
 
 # Number of workers for sending notifications
-config :alert_processor, notification_workers: 50
+config :alert_processor, notification_workers: 40
 
 # Config for db migration function
 config :alert_processor, :migration_task, AlertProcessor.ReleaseTasks.Dev
