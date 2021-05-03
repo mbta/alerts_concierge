0325570aacb41266dbe239de64a9f0550f286fc6
Cora Grant <394835+x@users.noreply.github.com>
2021-05-03 11:20:21 -0400

    test: update for new CR Spring schedules

--- apps/alert_processor/test/integration/matching_test.exs
+++ apps/alert_processor/test/integration/matching_test.exs
@@ -1210,9 +1210,9 @@ defmodule AlertProcessor.Integration.MatchingTest do
   # are active as of 2021-04-05.
 
   @test_trip_id (case Date.utc_today() |> Date.day_of_week() do
-                   day when day in 1..5 -> "CR-Weekday-Spring-21-909"
-                   6 -> "CR-Saturday-Spring-21-2907"
-                   7 -> "CR-Sunday-Spring-21-2907"
+                   day when day in 1..5 -> "CR-483969-909"
+                   6 -> "CR-478237-2907"
+                   7 -> "CR-478184-2907"
                  end)
   @test_trip_departs_fairmount_at (case Date.utc_today() |> Date.day_of_week() do
                                      day when day in 1..5 -> ~T[08:25:00]
