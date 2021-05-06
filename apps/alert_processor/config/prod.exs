23fc751fe08b28ac8e71c62d88953dafcc6277c7
Cora Grant <394835+x@users.noreply.github.com>
2021-05-06 10:41:43 -0400

    config: use default database pool size in prod
    We previously increased the pool size (see #860) to work around this
    issue: https://github.com/elixir-ecto/db_connection/issues/127
    
    As of recently (see #1129) we are on a version of OTP greater than the
    one that fixes the issue. Also, we are now starting to run the app on
    more than one instance at a time, and the large number of connections
    opened by the pool was causing additional instances to crash on startup
    with the error `FATAL 53300 (too_many_connections)`.

--- apps/alert_processor/config/prod.exs
+++ apps/alert_processor/config/prod.exs
@@ -2,9 +2,6 @@ use Mix.Config
 
 config :alert_processor, database_url: {:system, "DATABASE_URL_PROD"}
 
-# Configure your database
-config :alert_processor, AlertProcessor.Repo, pool_size: 50
-
 # Config for ExAws lib
 config :alert_processor, :ex_aws, ExAws
 
