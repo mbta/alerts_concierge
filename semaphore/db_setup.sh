6d5645e50c8e3d403293f494f5938d24fdb66389
Cora Grant <394835+x@users.noreply.github.com>
2021-06-23 18:08:25 -0400

    ci: delete Semaphore scripts and update docs

--- semaphore/db_setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-sem-service start postgres 10
-export DATABASE_URL_TEST="postgresql://$DATABASE_POSTGRESQL_USERNAME:$DATABASE_POSTGRESQL_PASSWORD@localhost:5432/alert_concierge_test"
