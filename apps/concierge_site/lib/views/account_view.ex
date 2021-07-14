7a34796b4c4161e9c742f2aff90527c22382c841
Cora Grant <394835+x@users.noreply.github.com>
2021-07-14 15:09:26 -0400

    fix: display "none" communication mode better
    Previously a `none` communication mode would be displayed the same as
    `email`. With this change, it will correctly result in neither of the
    options being selected. This also cleans up some minor inconsistencies
    between the "new user" settings form and the normal one.

--- apps/concierge_site/lib/views/account_view.ex
+++ apps/concierge_site/lib/views/account_view.ex
@@ -1,4 +1,13 @@
 defmodule ConciergeSite.AccountView do
   use ConciergeSite.Web, :view
   import ConciergeSite.PasswordHelper, only: [password_regex_string: 0]
+  alias AlertProcessor.Model.User
+  alias Ecto.Changeset
+
+  defp fetch_field!(changeset, field) do
+    {_, value} = Changeset.fetch_field(changeset, field)
+    value
+  end
+
+  defp sms_frozen?(%{data: user}), do: User.inside_opt_out_freeze_window?(user)
 end
