b3f50e8044d141f75ace20eff6b270c3db527554
Cora Grant <394835+x@users.noreply.github.com>
2021-09-09 12:18:28 -0400

    refactor: remove unused body utility classes
    * The `no-js` class was never removed, even when the browser had JS
      enabled (as might be expected), but it was also not used anywhere.
    
    * The `no-touch` class was similarly unused, so we can save the bit of
      JS that was necessary to apply it.

--- apps/concierge_site/assets/js/touch-support.js
+++
@@ -1,9 +0,0 @@
-export default () => {
-  const touchSupport =
-    "ontouchstart" in window ||
-    navigator.maxTouchPoints > 0 ||
-    navigator.msMaxTouchPoints > 0;
-  if (!touchSupport) {
-    document.body.className += " no-touch";
-  }
-};
