b7582f3c28f39cdb93b52c01045ff18442704f37
Cora Grant <394835+x@users.noreply.github.com>
2021-12-02 16:12:33 -0500

    wip: better SQL restrictions

--- apps/alert_processor/lib/model/saved_query.ex
+++ apps/alert_processor/lib/model/saved_query.ex
@@ -34,11 +34,79 @@ defmodule AlertProcessor.Model.SavedQuery do
   end
 
   @doc """
-  Executes the given query inside a transaction which is then rolled back, providing a basic
-  safeguard against changing any data.
+  Executes the given query. It is run inside a transaction that is then rolled back, using a
+  temporary role that only has SELECT permission, and only for columns that don't contain
+  personally identifying or sensitive information. Only one statement is allowed (thanks to
+  `Repo.query/1` using prepared statements), preventing the query from easily "escaping" the
+  restrictions, since `COMMIT`, `SET ROLE`, etc. will use up the single allowed statement.
+
+  These restrictions are intended primarily to avoid accidental data modification or exposure by
+  trusted administrators. They are *not* a guarantee that a sufficiently motivated hostile actor,
+  if allowed to pass arbitrary SQL to this function, could not somehow exploit it.
   """
   def execute(%__MODULE__{query: query}) do
-    {:error, result} = Repo.transaction(fn -> query |> Repo.query() |> Repo.rollback() end)
+    {:error, result} = Repo.transaction(fn -> query |> do_execute() |> Repo.rollback() end)
+    result
+  end
+
+  @permitted_columns [
+    {"alerts", :all},
+    {"informed_entities", :all},
+    {"metadata", :all},
+    {"notification_subscriptions", :all},
+    {"notifications", ~w(
+      id
+      user_id
+      alert_id
+      header
+      send_after
+      status
+      inserted_at
+      updated_at
+      last_push_notification
+      service_effect
+      description
+      url
+      closed_timestamp
+      type
+    )},
+    {"password_resets", :all},
+    {"saved_queries", :all},
+    {"trips", :all},
+    {"users", ~w(
+      id
+      inserted_at
+      updated_at
+      role
+      digest_opt_in
+      sms_opted_out_at
+      communication_mode
+      email_rejection_status
+    )},
+    {"versions", ~w(id event item_type item_id originator_id origin meta inserted_at)}
+  ]
+
+  defp do_execute(query) do
+    role = "temp_restricted_#{Ecto.UUID.generate() |> String.replace("-", "_")}"
+    Repo.query!("CREATE ROLE #{role}")
+
+    Enum.each(@permitted_columns, fn
+      {table, :all} ->
+        Repo.query!("GRANT SELECT ON #{table} TO #{role}")
+
+      {table, columns} when is_list(columns) ->
+        columns_for_grant = columns |> Enum.map(&~s("#{&1}")) |> Enum.join(",")
+        Repo.query!("GRANT SELECT (#{columns_for_grant}) ON #{table} TO #{role}")
+    end)
+
+    Repo.query!("SET LOCAL ROLE #{role}")
+    result = Repo.query(query)
+
+    # In case someone was trying to be clever and the query was a `COMMIT`, the role now exists
+    # for real, so we need to drop it
+    Repo.query!("DROP OWNED BY #{role}")
+    Repo.query!("DROP ROLE #{role}")
+
     result
   end
 end
