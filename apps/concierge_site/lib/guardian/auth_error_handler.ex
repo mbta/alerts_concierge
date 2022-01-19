50ccadaf7a5baecd8f2edb3c1483d2c32052b6c3
Cora Grant <394835+x@users.noreply.github.com>
2022-01-19 10:44:17 -0500

    fix: 500 error when auth token expired
    Guardian 0.14.x had a default set of error handlers it would fall back
    to if you didn't handle a particular type of error. This is no longer
    the case in Guardian 1.x due to the error handler API changing to use
    different clauses of a single callback, instead of multiple callbacks.
    This means we need to include a fallback clause ourselves.

--- apps/concierge_site/lib/guardian/auth_error_handler.ex
+++ apps/concierge_site/lib/guardian/auth_error_handler.ex
@@ -2,15 +2,16 @@ defmodule ConciergeSite.Guardian.AuthErrorHandler do
   @moduledoc false
   use ConciergeSite.Web, :controller
 
-  def auth_error(conn, {:unauthenticated, _reason}, _opts) do
-    conn
-    |> put_flash(:info, "Please log in.")
-    |> redirect(to: session_path(conn, :new))
-  end
-
   def auth_error(conn, {:unauthorized, _reason}, _opts) do
     conn
     |> put_flash(:error, "You are not authorized to view that page.")
-    |> redirect(to: "/")
+    |> redirect(to: trip_path(conn, :index))
+  end
+
+  def auth_error(conn, {_failure, _reason}, _opts) do
+    conn
+    |> configure_session(drop: true)
+    |> put_flash(:info, "Please log in.")
+    |> redirect(to: session_path(conn, :new))
   end
 end
