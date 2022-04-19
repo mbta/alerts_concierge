811030f8c596b4e14c22d132403949f088c0be8b
zovt <code@idylls.net>
2022-04-19 15:12:15 -0400

    chore: add abandonment rate admin query

--- apps/alert_processor/lib/model/query.ex
+++ apps/alert_processor/lib/model/query.ex
@@ -82,6 +82,58 @@ defmodule AlertProcessor.Model.Query do
         LEFT JOIN subscriptions s ON u.id = s.user_id
         GROUP BY u.communication_mode
         """
+      },
+      %__MODULE__{
+        label: "Weekly abandonment rate",
+        query: """
+        WITH
+          periods AS (
+            SELECT \"end\" - interval '1 week' AS start, \"end\"
+            FROM generate_series(
+              date_trunc('week', (SELECT MIN(inserted_at) FROM users)) - interval '1 second',
+              date_trunc('week', NOW() AT TIME ZONE 'utc') + interval '1 week' - interval '1 second',
+              interval '1 week'
+            ) \"end\"
+          ),
+          new_users_per_period AS (
+            SELECT COUNT(DISTINCT users.id) AS "count", periods.end AS period_end
+            FROM users
+            INNER JOIN periods ON (
+              periods.start <= users.inserted_at
+              AND users.inserted_at <= periods.end
+            )
+            WHERE
+              users.email_rejection_status IS NULL
+            GROUP BY
+              periods.end
+          ),
+          new_users_without_subscriptions_per_period AS (
+            SELECT COUNT(DISTINCT users.id) AS "count", periods.end AS period_end
+            FROM users
+            INNER JOIN periods ON (
+              periods.start <= users.inserted_at
+              AND users.inserted_at <= periods.end
+            )
+            LEFT JOIN subscriptions ON subscriptions.user_id = users.id
+            WHERE
+              users.email_rejection_status IS NULL
+              AND subscriptions.id IS NULL
+            GROUP BY
+              periods.end
+          )
+        SELECT
+          to_char(periods.end, 'YYYY-MM-DD') AS period_end,
+          COALESCE(new_users_per_period.count, 0) AS new_users,
+          COALESCE(new_users_without_subscriptions_per_period.count, 0) AS new_users_without_subscriptions
+        FROM periods
+        LEFT JOIN
+          new_users_per_period
+          ON new_users_per_period.period_end = periods.end
+        LEFT JOIN
+          new_users_without_subscriptions_per_period
+          ON new_users_without_subscriptions_per_period.period_end = periods.end
+        ORDER BY periods.end DESC
+        """
       }
     ]
     |> Enum.map(&%{&1 | id: &1.label |> String.downcase() |> String.replace(" ", "_")})
