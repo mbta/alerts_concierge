5cd2d7d99ab5810cf0fce7d48d34cf768d0cfccb
zovt <code@idylls.net>
2022-04-29 12:39:56 -0400

    fix: make it easier to splunk for opened notification email

--- apps/concierge_site/lib/controllers/email_opened_controller.ex
+++ apps/concierge_site/lib/controllers/email_opened_controller.ex
@@ -3,7 +3,10 @@ defmodule ConciergeSite.EmailOpenedController do
   require Logger
 
   def notification(conn, %{"alert_id" => alert_id, "notification_id" => notification_id}) do
-    Logger.info("email_opened alert_id=#{alert_id} notification_id=#{notification_id}")
+    Logger.info(
+      "email_opened=notification alert_id=#{alert_id} notification_id=#{notification_id}"
+    )
+
     conn = put_resp_content_type(conn, "image/gif", nil)
     send_resp(conn, 200, "")
   end
