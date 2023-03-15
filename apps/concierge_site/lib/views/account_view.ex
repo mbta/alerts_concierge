82b4709b63d1d5c90517f638870c58d68fe7ab36
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2023-03-15 14:15:55 -0400

    refactor: Make view functions used in templates public
    This is a personal preference. I find it confusing when these are
    private.

--- apps/concierge_site/lib/views/account_view.ex
+++ apps/concierge_site/lib/views/account_view.ex
@@ -3,10 +3,10 @@ defmodule ConciergeSite.AccountView do
   alias AlertProcessor.Model.User
   alias Ecto.Changeset
 
-  defp fetch_field!(changeset, field) do
+  def fetch_field!(changeset, field) do
     {_, value} = Changeset.fetch_field(changeset, field)
     value
   end
 
-  defp sms_frozen?(%{data: user}), do: User.inside_opt_out_freeze_window?(user)
+  def sms_frozen?(%{data: user}), do: User.inside_opt_out_freeze_window?(user)
 end
