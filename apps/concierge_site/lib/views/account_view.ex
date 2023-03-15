f028b6b6fd647a92cb6393419294fb6e53473a42
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2023-03-15 14:16:09 -0400

    feat: Handle password reset differently based on auth source

--- apps/concierge_site/lib/views/account_view.ex
+++ apps/concierge_site/lib/views/account_view.ex
@@ -2,6 +2,7 @@ defmodule ConciergeSite.AccountView do
   use ConciergeSite.Web, :view
   alias AlertProcessor.Model.User
   alias Ecto.Changeset
+  alias Plug.Conn
 
   def fetch_field!(changeset, field) do
     {_, value} = Changeset.fetch_field(changeset, field)
@@ -9,4 +10,26 @@ defmodule ConciergeSite.AccountView do
   end
 
   def sms_frozen?(%{data: user}), do: User.inside_opt_out_freeze_window?(user)
+
+  @spec edit_password_url(Conn.t()) :: String.t()
+  def edit_password_url(conn),
+    do: keycloak_auth_action_url("UPDATE_PASSWORD", encoded_account_edit_url(conn))
+
+  @spec keycloak_auth_action_url(String.t(), String.t()) :: String.t()
+  defp keycloak_auth_action_url(action, redirect_uri),
+    do:
+      "#{keycloak_base_uri()}/auth/realms/MBTA/protocol/openid-connect/auth?client_id=#{keycloak_client_id()}&kc_action=#{action}&response_type=code&redirect_uri=#{redirect_uri}"
+
+  @spec keycloak_base_uri :: String.t()
+  defp keycloak_base_uri, do: System.get_env("KEYCLOAK_BASE_URI")
+
+  @spec keycloak_client_id :: String.t()
+  defp keycloak_client_id, do: System.get_env("KEYCLOAK_CLIENT_ID")
+
+  @spec encoded_account_edit_url(Conn.t()) :: String.t()
+  defp encoded_account_edit_url(conn) do
+    conn
+    |> ConciergeSite.Router.Helpers.account_url(:edit)
+    |> URI.encode()
+  end
 end
