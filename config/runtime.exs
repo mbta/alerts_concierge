ae58962e8dc651623efbc14821001344820436d1
Erin Moore <erinmoorethefirst@gmail.com>
2024-02-08 09:17:37 -0500

    ssl configuration

--- config/runtime.exs
+++ config/runtime.exs
@@ -73,4 +73,19 @@ if config_env() == :prod do
   config :concierge_site,
     informizely_account_deleted_survey_id:
       System.fetch_env!("INFORMIZELY_ACCOUNT_DELETED_SURVEY_ID")
+
+  port = System.get_env("DATABASE_PORT") |> String.to_integer()
+  hostname = System.get_env("DATABASE_HOST")
+
+  config :alert_processor, AlertProcessor.Repo,
+    url: System.fetch_env!("DATABASE_URL_#{config_env() |> to_string() |> String.upcase()}"),
+    port: port,
+    ssl: true,
+    ssl_opts: [
+      cacertfile: "priv/aws-cert-bundle.pem",
+      verify: :verify_peer,
+      server_name_indication: String.to_charlist(hostname),
+      verify_fun:
+        {&:ssl_verify_hostname.verify_fun/3, [check_hostname: String.to_charlist(hostname)]}
+    ]
 end
