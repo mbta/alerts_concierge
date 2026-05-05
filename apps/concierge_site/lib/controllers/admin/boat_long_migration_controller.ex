3cc716dffe75ea9037d8fdc9b744b056169c5318
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2026-05-05 13:49:48 -0400

    format: mix format

--- apps/concierge_site/lib/controllers/admin/boat_long_migration_controller.ex
+++ apps/concierge_site/lib/controllers/admin/boat_long_migration_controller.ex
@@ -7,20 +7,26 @@ defmodule ConciergeSite.Admin.BoatLongMigrationController do
   alias AlertProcessor.Repo
 
   def index(conn, _params) do
-
     boat_f1_boat_long_count = subscription_count_for_route_and_stop("Boat-F1", "Boat-Long")
-    boat_eastboston_boat_long_count = subscription_count_for_route_and_stop("Boat-EastBoston", "Boat-Long")
-    boat_eastboston_boat_long_north_5b_count = subscription_count_for_route_and_stop("Boat-EastBoston", "Boat-Long-North-5B")
-    boat_lynn_boat_long_count = subscription_count_for_route_and_stop("Boat-Lynn", "Boat-Long")
-    boat_lynn_boat_long_north_5c_count = subscription_count_for_route_and_stop("Boat-Lynn", "Boat-Long-North-5C")
 
-    render(conn, "index.html", [
+    boat_eastboston_boat_long_count =
+      subscription_count_for_route_and_stop("Boat-EastBoston", "Boat-Long")
+
+    boat_eastboston_boat_long_north_5b_count =
+      subscription_count_for_route_and_stop("Boat-EastBoston", "Boat-Long-North-5B")
+
+    boat_lynn_boat_long_count = subscription_count_for_route_and_stop("Boat-Lynn", "Boat-Long")
+
+    boat_lynn_boat_long_north_5c_count =
+      subscription_count_for_route_and_stop("Boat-Lynn", "Boat-Long-North-5C")
+
+    render(conn, "index.html",
       boat_f1_boat_long_count: boat_f1_boat_long_count,
       boat_eastboston_boat_long_count: boat_eastboston_boat_long_count,
       boat_eastboston_boat_long_north_5b_count: boat_eastboston_boat_long_north_5b_count,
       boat_lynn_boat_long_count: boat_lynn_boat_long_count,
       boat_lynn_boat_long_north_5c_count: boat_lynn_boat_long_north_5c_count
-    ])
+    )
   end
 
   defp subscription_count_for_route_and_stop(route, stop) do
--- apps/concierge_site/test/web/controllers/admin/boat_long_migration_controller_test.exs
+++ apps/concierge_site/test/web/controllers/admin/boat_long_migration_controller_test.exs
@@ -19,27 +19,62 @@ defmodule BoatLongMigrationControllerTest do
       insert(:subscription, route: "Boat-F1", origin: "Boat-Hingham", destination: "Boat-Long")
       insert(:subscription, route: "Boat-F1")
 
-      insert(:subscription, route: "Boat-EastBoston", origin: "Boat-Long", destination: "Boat-Lewis")
-      insert(:subscription, route: "Boat-EastBoston", origin: "Boat-Lewis", destination: "Boat-Long")
+      insert(:subscription,
+        route: "Boat-EastBoston",
+        origin: "Boat-Long",
+        destination: "Boat-Lewis"
+      )
+
+      insert(:subscription,
+        route: "Boat-EastBoston",
+        origin: "Boat-Lewis",
+        destination: "Boat-Long"
+      )
+
       insert(:subscription, route: "Boat-EastBoston")
 
-      insert(:subscription, route: "Boat-EastBoston", origin: "Boat-Long-North-5B", destination: "Boat-Lewis")
-      insert(:subscription, route: "Boat-EastBoston", origin: "Boat-Lewis", destination: "Boat-Long-North-5B")
+      insert(:subscription,
+        route: "Boat-EastBoston",
+        origin: "Boat-Long-North-5B",
+        destination: "Boat-Lewis"
+      )
+
+      insert(:subscription,
+        route: "Boat-EastBoston",
+        origin: "Boat-Lewis",
+        destination: "Boat-Long-North-5B"
+      )
 
       insert(:subscription, route: "Boat-Lynn", origin: "Boat-Long", destination: "Boat-Blossom")
       insert(:subscription, route: "Boat-Lynn", origin: "Boat-Blossom", destination: "Boat-Long")
       insert(:subscription, route: "Boat-Lynn")
 
-      insert(:subscription, route: "Boat-Lynn", origin: "Boat-Long-North-5C", destination: "Boat-Blossom")
-      insert(:subscription, route: "Boat-Lynn", origin: "Boat-Blossom", destination: "Boat-Long-North-5C")
+      insert(:subscription,
+        route: "Boat-Lynn",
+        origin: "Boat-Long-North-5C",
+        destination: "Boat-Blossom"
+      )
+
+      insert(:subscription,
+        route: "Boat-Lynn",
+        origin: "Boat-Blossom",
+        destination: "Boat-Long-North-5C"
+      )
 
       conn = get(conn, admin_boat_long_migration_path(conn, :index))
 
       assert html_response(conn, 200) =~ ~r/<dd data-testid=\"boat-f1-boat-long-count\">2<\/dd>/
-      assert html_response(conn, 200) =~ ~r/<dd data-testid=\"boat-eastboston-boat-long-count\">2<\/dd>/
-      assert html_response(conn, 200) =~ ~r/<dd data-testid=\"boat-eastboston-boat-long-north-5b-count\">2<\/dd>/
+
+      assert html_response(conn, 200) =~
+               ~r/<dd data-testid=\"boat-eastboston-boat-long-count\">2<\/dd>/
+
+      assert html_response(conn, 200) =~
+               ~r/<dd data-testid=\"boat-eastboston-boat-long-north-5b-count\">2<\/dd>/
+
       assert html_response(conn, 200) =~ ~r/<dd data-testid=\"boat-lynn-boat-long-count\">2<\/dd>/
-      assert html_response(conn, 200) =~ ~r/<dd data-testid=\"boat-lynn-boat-long-north-5c-count\">2<\/dd>/
+
+      assert html_response(conn, 200) =~
+               ~r/<dd data-testid=\"boat-lynn-boat-long-north-5c-count\">2<\/dd>/
     end
 
     test "only available to admins", %{conn: conn} do
